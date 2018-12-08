de3b3c78628cc9fbe9a2b58bd0405f8c8eb4e660
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2018-12-08 19:36:57 +0000

    AoC Day 8 - Finally finished.....
    What a nightmare,..

--- 2018/Day 8/Program.cs
+++ 2018/Day 8/Program.cs
@@ -7,19 +7,13 @@ namespace Day_8
 {
     class Program
     {
+        private static int treeDataIndex = 0;
+
         static void Main(string[] args)
         {
-            // DataStructure:
-            //  A header, which is always exactly two numbers:
-            //      The quantity of child nodes.
-            //      The quantity of metadata entries.
-            //  Zero or more child nodes (as specified in the header).
-            //  One or more metadata entries (as specified in the header).
-
             var line = File.ReadAllLines("./input.txt")[0].Split(' ').Select(c => int.Parse(c)).ToList();
-            
-// var line = new [] {3, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 2, 1, 2, 1, 0, 1, 3, 0, 2, 4, 3, 7, 0, 1, 5, 1, 1, 1, 2}.ToList();
-//var line = new [] {2, 1, 2, 1, 0, 1, 3, 0, 2, 4, 3, 7, 0, 1, 5, 1}.ToList();
+            // var line = new [] {2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2}.ToList();
+            //var line = new [] {3, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 2, 1, 2, 1, 0, 1, 3, 0, 2, 4, 3, 2, 0, 1, 5, 1, 1, 1, 2}.ToList();
             var indices = new List<int>();
             GetLengthOfChunk(0, line, indices);
 
@@ -32,6 +26,10 @@ namespace Day_8
             }
             
             Console.WriteLine(total);
+
+            var root = CreateTree(indices, line);
+
+            Console.WriteLine(root.Value);
         }
 
         public static int GetLengthOfChunk(int currentIndex, List<int> list, List<int> indices)
@@ -70,10 +68,50 @@ namespace Day_8
             return length;
         }
 
+        public static Node CreateTree(List<int> nodeStartIndices, List<int> data)
+        {
+            Node root = new Node
+            {
+                Metadata = new List<int>(), 
+                Children = new List<Node>()
+            };
+
+            var nodeStartIndex = nodeStartIndices.First();
+            var numberOfChildren = data[nodeStartIndex];
+            var amountOfMetadata = data[nodeStartIndex + 1];
+            nodeStartIndices.RemoveAt(0);
+
+            while(numberOfChildren > 0)
+            {
+                root.Children.Add(CreateTree(nodeStartIndices, data));
+                numberOfChildren--;
+            }
+
+/*          if(nodeStartIndices.Any())
+            {
+                root.Metadata = data.GetRange(nodeStartIndices.First() - amountOfMetadata, amountOfMetadata);
+                treeDataIndex = nodeStartIndices.First();
+            }
+            else*/ if (data[nodeStartIndex] == 0) // initial number of children was 0, this is a leaf node
+            {
+                root.Metadata = data.GetRange(nodeStartIndex + 2, amountOfMetadata);
+                treeDataIndex = nodeStartIndex + 2 + amountOfMetadata;
+            }
+            else // edge case, this is the last node in the list, but not necessarily the top node
+            {
+                root.Metadata = data.GetRange(treeDataIndex, amountOfMetadata);
+                treeDataIndex += amountOfMetadata;
+            }
+
+            return root;
+        }
+
         public class Node
         {
             public List<int> Metadata { get; set; }
             public List<Node> Children{ get; set; }
+
+            public int Value => Children.Any() ? Metadata.Select(m => m > Children.Count() ? 0 : Children[m-1].Value).Sum() : Metadata.Sum();
         }
     }
 }
