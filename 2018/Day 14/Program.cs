4914e18d60127964a29e2f1559a0c99dbd29e634
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2018-12-14 09:24:43 +0000

    Day 14 - Fixed. TODO Make input generic

--- 2018/Day 14/Program.cs
+++ 2018/Day 14/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Day_14
 {
@@ -7,14 +8,13 @@ namespace Day_14
     {
         static void Main(string[] args)
         {
-            // var input = new List<int>{0, 4, 7, 8, 0, 1};
+            Stopwatch stopwatch = Stopwatch.StartNew();
             
             var input = new List<int> {0, 4, 7, 8, 0, 1};
 
             var state = new List<int> {3, 7};
             var firstElfIndex = 0;
             var secondElfIndex = 1;
-            var count = state.Count;
 
             while(!ShouldIFinish(input, state))
             {
@@ -26,7 +26,7 @@ namespace Day_14
                 }
                 state.Add(newScore % 10);
 
-                count = state.Count;
+                var count = state.Count;
                 firstElfIndex = (firstElfIndex + state[firstElfIndex] + 1) % count;
                 secondElfIndex = (secondElfIndex + state[secondElfIndex] + 1) % count;
             }
@@ -40,21 +40,37 @@ namespace Day_14
 
             Console.WriteLine("Number of Recipes");
             Console.WriteLine(state.Count - input.Count - 1);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            Console.ReadLine();
         }
 
         public static bool ShouldIFinish(List<int> input, List<int> state){
             bool result = true;
 
             if (state.Count < input.Count + 1){
-                result = false;
-                return result;
+                return false;
             }
 
-            for(int i = 1; i <= input.Count; i++){
+            for(int i = 1; i <= input.Count; i++)
+            {
                 if (state[state.Count - i] != input[input.Count - i] && state[state.Count - 1 - i] != input[input.Count - i])
                 {
                     result = false;
                 }
+            }
+
+            if (result)
+            {
+                string stringState = "";
+                for (int i = -7; i < 0; i++)
+                {
+                    stringState += state[state.Count + i];
+                }
+
+                var inputString = input[0].ToString() + input[1] + input[2] + input[3] + input[4] + input[5];
+
+                result = stringState.Contains(inputString);
             }
 
             return result;
