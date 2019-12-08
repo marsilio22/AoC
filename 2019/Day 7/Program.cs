962274ca7175079812279fc3dea5296eb7517f17
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2019-12-08 22:33:18 +0000

    Added Comments and restructure

--- 2019/Day 7/Program.cs
+++ 2019/Day 7/Program.cs
@@ -6,78 +6,39 @@ using System.Linq;
 namespace Day_7 {
     class Program {
         static void Main (string[] args) {
-            var program = File.ReadAllLines ("./input.txt") [0].Split (",").Select (i => int.Parse (i)).ToList();
-            //var program = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0".Split (",").Select (i => int.Parse (i)).ToList();
+            var program = File.ReadAllLines ("./input.txt") [0].Split (",").Select (i => int.Parse (i)).ToList ();
 
             // Part 1
-            Part1(program);
+            //var program = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0".Split (",").Select (i => int.Parse (i)).ToList();
+            Part1 (program, new List<int> { 0, 1, 2, 3, 4 });
 
             // Part 2
-            
             // solution permutation 98765 = 139629729
             //program = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5".Split (",").Select (i => int.Parse (i)).ToList();
 
             // solution permutation 97856 = 18216
             //program = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10".Split (",").Select (i => int.Parse (i)).ToList();
-            
-            var phases = new List<int> { 5, 6, 7, 8, 9 };
-            //var permutations = new List<List<int>>{new List<int>{9,8,7,6,5}};
-            var permutations = GetPermutations (phases, 5).ToList ();
 
-            var nextInput = 0;
-            var largestOutput = int.MinValue;
-
-            foreach (var phasePermutation in permutations) {
-                var amplifierPrograms = new Dictionary<int, (List<int> ampProgram, int? currentIndex)> {
-                    { 0, (program.ToList (), null)}, // amp A
-                    { 1, (program.ToList (), null)},
-                    { 2, (program.ToList (), null)},
-                    { 3, (program.ToList (), null)},
-                    { 4, (program.ToList (), null)}  // amp E
-                };
-                nextInput = 0;
-                int i = 0;
-                while(true){
-                    var input = new Queue<int>();
-                    if(amplifierPrograms[i].currentIndex == null){
-                        input.Enqueue(phasePermutation.ToList()[i]);
-                        var prog = amplifierPrograms[i];
-                        prog.currentIndex = 0;
-                        amplifierPrograms[i] = prog;
-                    }
-                    input.Enqueue(nextInput);
-
-                    var returnValue = Calculate(amplifierPrograms[i].ampProgram, input, amplifierPrograms[i].currentIndex.Value);
-                    nextInput = returnValue.returnValue;
-
-                    if (returnValue.didHalt && i == 4){
-                        break;
-                    }
-                    else
-                    {
-                        var ampProg = amplifierPrograms[i];
-                        amplifierPrograms[i] = (ampProg.ampProgram, returnValue.currentIndex);
-                        i = (i + 1) % amplifierPrograms.Count;
-                    }
-                }
-                
-                largestOutput = largestOutput < nextInput ? nextInput : largestOutput;
-                Console.WriteLine (nextInput);
-            }
-
-            Console.WriteLine(largestOutput);
+            Part2 (program, new List<int> { 5, 6, 7, 8, 9 });
         }
 
-        public static void Part1(ICollection<int> program){
-            var phases = new List<int> { 0, 1, 2, 3, 4 };
+        /// <summary>
+        /// Calculate part 1
+        /// </summary>
+        /// <param name="program">The program</param>
+        /// <param name="phases">The valid phases to permute through</param>
+        public static void Part1 (ICollection<int> program, List<int> phases) {
             var permutations = GetPermutations (phases, 5).ToList ();
-
             var nextInput = 0;
             var largestOutput = int.MinValue;
 
+            // Find which permutation produces the largest output 
             foreach (var phasePermutation in permutations) {
                 nextInput = 0;
                 foreach (var amplifier in phasePermutation) {
+                    // queue input, to be dequeued by the input commands
+                    // First param is the amplifier phase, second is the output from 
+                    // the previous amplifier, or 0 if this is the first amplifier.
                     var input = new Queue<int> ();
                     input.Enqueue (amplifier);
                     input.Enqueue (nextInput);
@@ -86,21 +47,82 @@ namespace Day_7 {
                 }
 
                 largestOutput = largestOutput < nextInput ? nextInput : largestOutput;
-                Console.WriteLine (nextInput);
             }
 
-            Console.WriteLine ();
             Console.WriteLine (largestOutput);
-            Console.WriteLine ("------------------");
+        }
+
+        /// <summary>
+        /// Calculate part 2
+        /// </summary>
+        /// <param name="program">The program</param>
+        /// <param name="phases">The valid phases to permute through</param>
+        public static void Part2 (ICollection<int> program, List<int> phases) {
+            var permutations = GetPermutations (phases, 5).ToList ();
+
+            var nextInput = 0;
+            var largestOutput = int.MinValue;
+
+            // This time there is a feedback loop, Amplifier E inputs to Amplifier A, until all have halted.
+            foreach (var phasePermutation in permutations) {
+                // Programs are not restarted, so keep track of their individual states, and current index
+                var amplifierPrograms = new Dictionary < int,
+                    (List<int> ampProgram, int? currentIndex) > { { 0, (program.ToList (), null) }, // amp A
+                        { 1, (program.ToList (), null) },
+                        { 2, (program.ToList (), null) },
+                        { 3, (program.ToList (), null) },
+                        { 4, (program.ToList (), null) } // amp E
+                    };
+
+                nextInput = 0;
+                int i = 0;
+
+                while (true) {
+                    var input = new Queue<int> ();
+                    // We only want to add the Phase to the input queue if this is the first time the program has been run
+                    if (amplifierPrograms[i].currentIndex == null) {
+                        input.Enqueue (phasePermutation.ToList () [i]);
+                        var prog = amplifierPrograms[i];
+                        prog.currentIndex = 0;
+
+                        amplifierPrograms[i] = prog;
+                    }
+                    // We always want to input the next value (initialised to 0 for the first run)
+                    input.Enqueue (nextInput);
+
+                    var calculationResult = Calculate (amplifierPrograms[i].ampProgram, input, amplifierPrograms[i].currentIndex.Value);
+                    nextInput = calculationResult.returnValue;
+
+                    if (calculationResult.didHalt && i == 4) {
+                        // Stop if this is Amplifier E (index 4) and the calculation halted (rather than awaiting new input)
+                        break;
+                    } else {
+                        // otherwise, update the current index of this amplifier, and iterate i to the next amplifier
+                        var ampProg = amplifierPrograms[i];
+                        amplifierPrograms[i] = (ampProg.ampProgram, calculationResult.currentIndex);
+                        i = (i + 1) % amplifierPrograms.Count;
+                    }
+                }
+
+                // Keep track of the largest output
+                largestOutput = largestOutput < nextInput ? nextInput : largestOutput;
+            }
+
+            Console.WriteLine (largestOutput);
         }
 
         /// <summary>
         /// Calculates the output state of the given set of codes, for the given noun and verb
         /// </summary>
-        /// <returns>The value at index 0, after the program has run</returns>
-        public static (int returnValue, bool didHalt, bool requiresInput, int currentIndex) Calculate (List<int> intcode, Queue<int> input, int startingIndex) {
+        /// <returns>
+        /// A tuple containing: 
+        /// 1. The return value, if any
+        /// 2. A value indicating whether or not the process has halted (if not then it is awaiting more input)
+        /// 3. The value of the current index of the running program, if appropriate
+        /// </returns>
+        public static (int returnValue, bool didHalt, int currentIndex) Calculate (List<int> intcode, Queue<int> input, int startingIndex) {
             var returnValue = 0;
-            for (int i = startingIndex; /*intcode[i] != 99*/;) {
+            for (int i = startingIndex;;) {
                 var opcode = intcode[i].ToString ();
                 string instruction, modes;
                 if (opcode.Length > 2) {
@@ -133,18 +155,18 @@ namespace Day_7 {
                         i += 4;
                         break;
                     case 3: // Input
-                        if (!input.Any()){
-                            return (returnValue, false, true, i);
+                        if (!input.Any ()) {
+                            return (returnValue, false, i);
                         }
                         intcode[intcode[i + 1]] = input.Dequeue ();
                         i += 2;
                         break;
                     case 4: // Output
-                        Console.WriteLine (intcode[intcode[i + 1]]);
-                        returnValue = intcode[intcode[i+1]];
-                        i+=2;
+                        // For this day, don't actually return at this point, because we need to carry on
+                        // until we reach either a halt, or a 99.
+                        returnValue = intcode[intcode[i + 1]];
+                        i += 2;
                         break;
-                        //return (intcode[intcode[i + 1]], intcode[i + 2] == 99);
                     case 5: // Jump if true
                         int test = modeC == 0 ? intcode[intcode[i + 1]] : intcode[i + 1];
                         i = test == 0 ? i + 3 : (modeB == 0 ? intcode[intcode[i + 2]] : intcode[i + 2]);
@@ -166,8 +188,7 @@ namespace Day_7 {
                         i += 4;
                         break;
                     case 99:
-                        //Console.WriteLine ("Found a 99");
-                        return (returnValue, true, false, 0); // 0 is questionable
+                        return (returnValue, true, 0);
                     default:
                         throw new ArgumentException ();
                 }
