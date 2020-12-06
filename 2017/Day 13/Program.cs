85cc4579d079721f524bfef50026e25071b48281
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2020-12-06 06:18:27 +0000

    2017 - Day 13

---
+++ 2017/Day 13/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines("./input.txt").ToList();
+
+            var part1 = CalculateSeverity(lines, 0, false);
+
+            Console.WriteLine(part1);
+
+            var part2 = part1;
+            var delay = 0;
+
+            while(part2 != 0)
+            {
+                delay++;
+                part2 = CalculateSeverity(lines, delay, true);
+                
+            }
+
+            Console.WriteLine(delay); 
+        }
+
+        public static int CalculateSeverity(ICollection<string> lines, int startingDelay, bool part2)
+        {
+            int ans = 0;
+            foreach(var line in lines)
+            {
+                var depthRange = line.Split(":").Select(n => int.Parse(n)).ToList();
+
+                if ((depthRange[0] + startingDelay) % ((depthRange[1] * 2) - 2)  == 0)
+                {
+                    ans += depthRange[0] * depthRange[1];
+                    
+                    // if we ever get caught in part 2, return, to save time on running lots
+                    if (part2)
+                    {
+                        return 1;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
