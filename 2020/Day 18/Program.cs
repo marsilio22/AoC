fa6d8ca7f6f43d2142bd32b681e983967e94b411
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2020-12-18 11:28:16 +0000

    2020 - Day 18 finish

--- 2020/Day 18/Program.cs
+++ 2020/Day 18/Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
 using System.Linq;
-using System.Text;
-using Microsoft.CSharp;
-using System.CodeDom;
 using System.Text.RegularExpressions;
 
 namespace Day_18
@@ -14,113 +10,35 @@ namespace Day_18
     {
         static void Main(string[] args)
         {
-            var lines = File.ReadLines("testinput.txt");
+            var lines = File.ReadLines(@"./input.txt").ToList();
             long ans = 0;
 
-            foreach(var line in lines){
+            foreach (string line in lines)
+            {
                 int i = 0;
                 ans += DoMathsPart1(line, ref i);
             }
 
-            Console.WriteLine(ans);
+            Console.WriteLine("Part 1: " + ans);
 
-            // part 2 
-            var nonNumericChars = new [] {'(', ')', '+', '*'};
-
-            // foreach(var line in lines){
-            //     while (line.Any(l => nonNumericChars.Contains(l)))
-            //     {
-            //         //sort out the lowest level of bracketed numbers
-            //     }
-            // }
-
-            var additionRegex = new Regex(@"\d+ \+ \d+");
-            var multiplicationRegex = new Regex(@"\d+ \* \d+");
-            var multiplicationInBracketsRegex = new Regex(@"\(\d+ \* \d+");
-            var multiplicationInBracketsFollowedImmediatelyByAnAdditionToABracketRegex = new Regex(@"\(\d+ \* \d+ \+ \(");
-
-            var singleNumberSurroundedByBrackets = new Regex(@"\(\d+\)");
-            foreach(var line in lines){
-                var lineCopy = line;
-                Console.WriteLine(lineCopy);
-
-                while (lineCopy.Any(l => nonNumericChars.Contains(l))){
-                    while(additionRegex.Matches(lineCopy).Count > 0)
-                    {
-                        var test = additionRegex.Match(lineCopy);
-                        var spl = test.Value.Split(" + ");
-                        var result = long.Parse(spl[0]) + long.Parse(spl[1]);
-
-                        lineCopy = lineCopy.Replace(test.Value, result.ToString());
-                Console.WriteLine(lineCopy);
-                    }
-
-                    if (!lineCopy.Contains('+'))
-                    {
-                        while(multiplicationRegex.Matches(lineCopy).Count > 0)
-                        {
-                            var test = multiplicationRegex.Match(lineCopy);
-                            var spl = test.Value.Split(" * ");
-                            var result = long.Parse(spl[0]) * long.Parse(spl[1]);
-
-                            lineCopy = lineCopy.Replace(test.Value, result.ToString());
-                Console.WriteLine(lineCopy);
-                        }
-                    }
-                    else 
-                    {
-                        // argh this is wrong I think
-                        // need to do the multiplication when the regex matches 
-                        // "things that are in brackets that need multiplying, where the brackets contain no other open bracket characters
-            var myThought = new Regex(@"\(\d+ \* ?\d+ ?\*? ?\d* ?\*? ?\d* ?\*? ?\d*\)");
-            var t = myThought.Match(lineCopy);
-                        while(myThought.Matches(lineCopy).Count > 0)
-                        {
-                            var test = multiplicationRegex.Match(lineCopy);
-                            var spl = test.Value.Split(" * ");
-                            var result = long.Parse(spl[0]) * long.Parse(spl[1]);
-
-                            lineCopy = lineCopy.Replace(spl[0] + " * " + spl[1], result.ToString());
-                Console.WriteLine(lineCopy);
-                        }
-                //         while(multiplicationInBracketsRegex.Matches(lineCopy).Count > 0 && 
-                //             !multiplicationInBracketsFollowedImmediatelyByAnAdditionToABracketRegex.Match(lineCopy).Value.Contains(multiplicationInBracketsRegex.Match(lineCopy).Value))
-                //         {
-                //             var matchesWeDontWantToCompute = multiplicationInBracketsFollowedImmediatelyByAnAdditionToABracketRegex.Matches(lineCopy).Select(m => m.Value);
-                //             var test = multiplicationInBracketsRegex.Matches(lineCopy).First(m => !matchesWeDontWantToCompute.Any(m2 => m2.Contains(m.Value)));
-
-                //             var spl = test.Value.Split('(')[1].Split(" * ");
-                //             var result = long.Parse(spl[0]) * long.Parse(spl[1]);
-
-                //             lineCopy = lineCopy.Replace(test.Value.Split('(')[1], result.ToString());
-                // Console.WriteLine(lineCopy);
-                //         }
-                    }
-
-                    while(singleNumberSurroundedByBrackets.Matches(lineCopy).Count > 0)
-                    {
-                        var test = singleNumberSurroundedByBrackets.Match(lineCopy);
-                        var spl = test.Value.Split('(')[1].Split(')')[0];
-
-                        lineCopy = lineCopy.Replace(test.Value, spl);
-                Console.WriteLine(lineCopy);
-                    }
-                }
-                
-                Console.WriteLine(lineCopy);
-                Console.WriteLine();
+            ans = 0;
+            foreach (string line in lines)
+            {
+                ans += DoMathsPart2(line, false);
             }
+
+            Console.WriteLine("Part 2:" + ans);
         }
 
         public static long DoMathsPart1(string maths, ref int i)
         {
             long ans = 0;
-            var operation = '+'; // initial operation to make ans be the first value
+            char operation = '+'; // initial operation to make ans be the first value
 
-            for(; i< maths.Length; i++)
+            for (; i < maths.Length; i++)
             {
-                var character = maths[i];
-                if(character == ')')
+                char character = maths[i];
+                if (character == ')')
                 {
                     return ans;
                 }
@@ -143,8 +61,10 @@ namespace Day_18
             return ans;
         }
 
-        public static long DoSingleOp(long a, long b, char op){
-            switch(op){
+        public static long DoSingleOp(long a, long b, char op)
+        {
+            switch (op)
+            {
                 case '+':
                     return a + b;
                 case '*':
@@ -152,27 +72,82 @@ namespace Day_18
             }
             throw new Exception("Shouldn't happen");
         }
+
+        public static long DoMathsPart2(string line, bool log)
+        {
+            char[] nonNumericChars = new[] { '(', ')', '+', '*' };
+
+            Regex additionRegex = new Regex(@"\d+ \+ \d+");
+            Regex multiplicationRegex = new Regex(@"\d+ \* \d+");
+            Regex multiplicationsInABracketRegex = new Regex(@"\(\d+ \* ?\d+ ?\*? ?\d* ?\*? ?\d* ?\*? ?\d* ?\*? ?\d* ?\*? ?\d* ?\*? ?\d* ?\*? ?\d* ?\*? ?\d*\)");
+            Regex singleNumberSurroundedByBrackets = new Regex(@"\(\d+\)");
+
+            string lineCopy = line;
+
+            if (log) 
+                Console.WriteLine(lineCopy);
+
+            while (lineCopy.Any(l => nonNumericChars.Contains(l)))
+            {
+                while (additionRegex.Matches(lineCopy).Count > 0)
+                {
+                    Match test = additionRegex.Match(lineCopy);
+                    string[] spl = test.Value.Split(" + ");
+                    long result = long.Parse(spl[0]) + long.Parse(spl[1]);
+
+                    lineCopy = lineCopy.Substring(0, test.Index) + result +
+                               lineCopy.Substring(test.Index + test.Value.Length);
+
+                    if (log)
+                        Console.WriteLine(lineCopy);
+                }
+
+                if (!lineCopy.Contains('+'))
+                {
+                    while (multiplicationRegex.Matches(lineCopy).Count > 0)
+                    {
+                        Match test = multiplicationRegex.Match(lineCopy);
+                        string[] spl = test.Value.Split(" * ");
+                        long result = long.Parse(spl[0]) * long.Parse(spl[1]);
+
+                        lineCopy = lineCopy.Substring(0, test.Index) + result +
+                                   lineCopy.Substring(test.Index + test.Value.Length);
+
+                        if (log)
+                            Console.WriteLine(lineCopy);
+                    }
+                }
+                else
+                {
+                    while (multiplicationsInABracketRegex.Matches(lineCopy).Count > 0)
+                    {
+                        Match test = multiplicationsInABracketRegex.Match(lineCopy);
+                        List<string> spl = test.Value.Split(" * ").Select(c => c.Split('(').Last()).Select(c => c.Split(')').First()).ToList();
+                        long result = long.Parse(spl[0]) * long.Parse(spl[1]);
+                        string replstr = spl[0] + " * " + spl[1];
+
+                        lineCopy = lineCopy.Substring(0, test.Index + 1) + result +
+                                   lineCopy.Substring(test.Index + replstr.Length + 1);
+
+                        if (log)
+                            Console.WriteLine(lineCopy);
+                    }
+                }
+
+                while (singleNumberSurroundedByBrackets.Matches(lineCopy).Count > 0)
+                {
+                    Match test = singleNumberSurroundedByBrackets.Match(lineCopy);
+                    string spl = test.Value.Split('(')[1].Split(')')[0];
+
+                    lineCopy = lineCopy.Substring(0, test.Index) + spl +
+                               lineCopy.Substring(test.Index + test.Value.Length);
+
+                    if (log)
+                        Console.WriteLine(lineCopy);
+                }
+            }
+
+            return long.Parse(lineCopy);
+        }
     }
-
-    // public class MyNumber
-    // {
-    //     private long v;
-
-    //     public MyNumber(long v)
-    //     {
-    //         this.value = v;
-    //     }
-
-    //     public long value {get;set;}
-
-    //     public static MyNumber operator +(MyNumber a, MyNumber b)
-    //     {
-    //         return new MyNumber(a.value*b.value);
-    //     }
-
-    //     public static MyNumber operator *(MyNumber a, MyNumber b)
-    //     {
-    //         return new MyNumber(a.value + b.value);
-    //     }
-    // }
 }
