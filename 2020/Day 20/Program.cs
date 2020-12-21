13da60b611e29f8e75c48ff0a4e5ee4dbfd03c28
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2020-12-21 11:50:50 +0000

    Day 20 - Done...

--- 2020/Day 20/Program.cs
+++ 2020/Day 20/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Day_20
 {
@@ -9,7 +11,7 @@ namespace Day_20
     {
         static void Main(string[] args)
         {
-            var lines = File.ReadLines("./testinput.txt").ToList();
+            var lines = File.ReadLines("./input.txt").ToList();
             var tiles = new Dictionary<int, IDictionary<(int x, int y), char>>();
             int num = 0;
 
@@ -18,7 +20,7 @@ namespace Day_20
                 var line = lines[i];
 
                 if (line.StartsWith("Tile"))
-                {
+                { 
                     num = int.Parse(line.Split(" ")[1].Split(':')[0]);
                     tiles.Add(num, new Dictionary<(int, int), char>());
                 }
@@ -197,12 +199,12 @@ namespace Day_20
             
             // pick a random tile in the middle. 
             // This tile will determine the orientation by which we assemble the tiles.
-            var currentTile = adjacents.First(c => c.Value.Count == 4); 
+            var t1 = adjacents.First(c => c.Value.Count == 4); 
 
-            tileMap.Add(currentTile.Key, ((0, 0), new D4(D4_element.E), 'a'));
+            tileMap.Add(t1.Key, ((0, 0), new D4(D4_element.E), 'a'));
 
             var tilesToDo = new Queue<KeyValuePair<int, ICollection<(int tileId, string facing, D4 operation)>>>();
-            tilesToDo.Enqueue(currentTile);
+            tilesToDo.Enqueue(t1);
 
             while(tilesToDo.Any())
             {
@@ -346,64 +348,143 @@ namespace Day_20
                 }
             }
 
+
+            var firstTile = adjacents.First();
+            
+            var tileMap2 = new Dictionary<(int x, int y), (int tileId, IDictionary<(int x, int y), char> tileValues)>();
+            tileMap2.Add((0, 0), (firstTile.Key, tiles[firstTile.Key]));
+
+            var queue = new Queue<KeyValuePair<int, ICollection<(int tileId, string direction, D4 op)>>>();
+
+            var operations = new List<D4>
+            {
+                new D4(D4_element.E),
+                new D4(D4_element.R),
+                new D4(D4_element.RR),
+                new D4(D4_element.RRR),
+                new D4(D4_element.F),
+                new D4(D4_element.RF),
+                new D4(D4_element.RRF),
+                new D4(D4_element.RRRF)
+            };
+
+            queue.Enqueue(firstTile);
+
+            while(queue.Any())
+            {
+                // get this tile
+                var currentTile = queue.Dequeue();
+                var tile = tileMap2.Single(t => t.Value.tileId == currentTile.Key);
+                var adjTileIds = currentTile.Value.Select(t => t.tileId).ToList();
+
+                // for each adjacent to this tile
+                var adjs = tiles.Where(t => adjTileIds.Contains(t.Key) && !tileMap2.Any(t2 => t2.Value.tileId == t.Key));
+
+                // find the rotary operation which matches an unused edge
+                var topEdge = string.Join(null, tile.Value.tileValues.Where(v => v.Key.y == 0).Select(c => c.Value).ToList());
+                var bottomEdge = string.Join(null, tile.Value.tileValues.Where(v => v.Key.y == 9).Reverse().Select(c => c.Value).ToList());
+                var leftEdge = string.Join(null, tile.Value.tileValues.Where(v => v.Key.x == 0).Reverse().Select(c => c.Value).ToList());
+                var rightEdge = string.Join(null, tile.Value.tileValues.Where(v => v.Key.x == 9).Select(c => c.Value).ToList());
+
+                var currentTileEdges = new Dictionary<string, string>{
+                    { "top", topEdge },
+                    { "bottom", bottomEdge },
+                    { "left", leftEdge },
+                    { "right", rightEdge }
+                };
+
+                var currentTileCoord = tileMap2.Single(t => t.Value.tileId == currentTile.Key).Key;
+
+                foreach(var otherTile in adjs)
+                {
+                    // try every operation until we get a match
+                    foreach(var operation in operations)
+                    {
+                        // rotate and reflect under the operation
+                        var otherTile2 = Rotate(otherTile.Value, operation, 'x');
+                        
+                        // get the new edges. NB we read them backwards (anticlockwise) so that the strings will match 
+                        // the forwards (clockwise) read edges of the currentTile
+                        var otherTopEdge = string.Join(null, otherTile2.Where(v => v.Key.y == 0).Reverse().Select(c => c.Value).ToList());
+                        var otherBottomEdge = string.Join(null, otherTile2.Where(v => v.Key.y == 9).Select(c => c.Value).ToList());
+                        var otherLeftEdge = string.Join(null, otherTile2.Where(v => v.Key.x == 0).Select(c => c.Value).ToList());
+                        var otherRightEdge = string.Join(null, otherTile2.Where(v => v.Key.x == 9).Reverse().Select(c => c.Value).ToList());
+
+                        // put that tile in tilemap, in that orientation, at the right coord.
+                        if (otherTopEdge.Equals(currentTileEdges["bottom"]))
+                        {
+                            tileMap2.Add((currentTileCoord.x, currentTileCoord.y + 1), (otherTile.Key, otherTile2));
+                            queue.Enqueue(adjacents.Single(kvp => kvp.Key == otherTile.Key));
+                            break;
+                        }
+                        else if (otherBottomEdge.Equals(currentTileEdges["top"]))
+                        {
+                            tileMap2.Add((currentTileCoord.x, currentTileCoord.y - 1), (otherTile.Key, otherTile2));
+                            queue.Enqueue(adjacents.Single(kvp => kvp.Key == otherTile.Key));
+                            break;
+                        }
+                        else if (otherRightEdge.Equals(currentTileEdges["left"]))
+                        {
+                            tileMap2.Add((currentTileCoord.x - 1, currentTileCoord.y), (otherTile.Key, otherTile2));
+                            queue.Enqueue(adjacents.Single(kvp => kvp.Key == otherTile.Key));
+                            break;
+                        }
+                        else if (otherLeftEdge.Equals(currentTileEdges["right"]))
+                        {
+                            tileMap2.Add((currentTileCoord.x + 1, currentTileCoord.y), (otherTile.Key, otherTile2));
+                            queue.Enqueue(adjacents.Single(kvp => kvp.Key == otherTile.Key));
+                            break;
+                        }
+                        else 
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+
+
+
             // now we have the map tiles in place, we need to perform the operations on them, and 
             // stitch them into the big map.
             var x = 0;
             var y = 0;
 
-            var iMin = tileMap.Values.Select(v => v.coord.y).Min(); // probably -6
-            var iMax = tileMap.Values.Select(v => v.coord.y).Max(); // probably 6
-            var jMin = tileMap.Values.Select(v => v.coord.x).Min(); // as above
-            var jMax = tileMap.Values.Select(v => v.coord.x).Max();
+            var iMin = tileMap2.Keys.Select(v => v.y).Min(); // probably -6
+            var iMax = tileMap2.Keys.Select(v => v.y).Max(); // probably 6
+            var jMin = tileMap2.Keys.Select(v => v.x).Min(); // as above
+            var jMax = tileMap2.Keys.Select(v => v.x).Max();
 
             var bigPicture = new Dictionary<(int x, int y), char>();
 
-            for (int i = iMin; i <= iMax; i++)
-            {
-                for (int j = jMin; j <= jMax; j++)
-                {
-                    if (tileMap.Any(t => t.Value.coord == (j, i)))
-                    {
-                        Console.Write('#');
-                    }
-                    else
-                    {
-                        Console.Write('.');
-                    }
-                }
-                Console.WriteLine();
-            }
-
-            var coords = tileMap.Select(c => c.Value.coord).Distinct();
-
-            foreach(var coord in coords){
-                Console.WriteLine($"for coord ({coord.x}, {coord.y}), there are {tileMap.Count(c => c.Value.coord == coord)} tiles");
-            }
-
-            Console.WriteLine();
-
-            var emptyDict = new Dictionary<(int, int), char>();
-            for (int i = 0; i < 10; i++){
-                for (int j = 0; j < 10; j++)
-                    emptyDict[(i, j)] = '.';
-            }
+            // for (int i = iMin; i <= iMax; i++)
+            // {
+            //     for (int j = jMin; j <= jMax; j++)
+            //     {
+            //         if (tileMap.Any(t => t.Value.coord == (j, i)))
+            //         {
+            //             Console.Write('#');
+            //         }
+            //         else
+            //         {
+            //             Console.Write('.');
+            //         }
+            //     }
+            //     Console.WriteLine();
+            // }
 
             for (int i = iMin; i <= iMax; i++)
             {
                 for (int j = jMin; j <= jMax; j++)
                 {
-                    var tile = tileMap.Single(t => t.Value.coord == (j, i));
-
-                    // rotate the tile in the (j, i) coord of tilemap under it's rotation
-                    // read out the values from that tile into bigPicture(x,y)
-
-                    var values = Rotate(tiles[tile.Key], tile.Value.operationForOrientation, tile.Value.reflectionAxis);
+                    var tile = tileMap2.Single(t => t.Key == (j, i)).Value.tileValues;
 
                     for (int k = 0; k < 10; k++)
                     {
                         for (int t = 0; t < 10; t++)
                         {
-                            bigPicture[(k + x, t + y)] = values[(k, t)];
+                            // bigPicture[(k + x, t + y)] = tile[(k, t)];
+                            bigPicture[(k + x, t + y)] = tile[(k, t)];
                         }
                     }
                     x += 10;
@@ -412,22 +493,144 @@ namespace Day_20
                 x = 0;
             }
 
+            // var coords = tileMap.Select(c => c.Value.coord).Distinct();
+
+            // foreach(var coord in coords){
+            //     Console.WriteLine($"for coord ({coord.x}, {coord.y}), there are {tileMap.Count(c => c.Value.coord == coord)} tiles");
+            // }
+
+            // Console.WriteLine();
+
+            // var emptyDict = new Dictionary<(int, int), char>();
+            // for (int i = 0; i < 10; i++){
+            //     for (int j = 0; j < 10; j++)
+            //         emptyDict[(i, j)] = '.';
+            // }
+
+
+
+
+
+
+
+
+            // for (int i = iMin; i <= iMax; i++)
+            // {
+            //     for (int j = jMin; j <= jMax; j++)
+            //     {
+            //         var tile = tileMap.Single(t => t.Value.coord == (j, i));
+
+            //         // rotate the tile in the (j, i) coord of tilemap under it's rotation
+            //         // read out the values from that tile into bigPicture(x,y)
+
+            //         var values = Rotate(tiles[tile.Key], tile.Value.operationForOrientation, tile.Value.reflectionAxis);
+
+            //         for (int k = 0; k < 10; k++)
+            //         {
+            //             for (int t = 0; t < 10; t++)
+            //             {
+            //                 bigPicture[(k + x, t + y)] = values[(k, t)];
+            //             }
+            //         }
+            //         x += 10;
+            //     }
+            //     y += 10;
+            //     x = 0;
+            // }
+
             PrintDict(bigPicture);
+            PrintDictToFile(bigPicture);
+
+            // remove the edges
+            x = 0;
+            y = 0;
+
+            iMax = bigPicture.Select(c => c.Key.y).Max();
+            jMax = bigPicture.Select(c => c.Key.x).Max();
+
+            var edgeCutBigPicture = new Dictionary<(int x, int y), char>();
+
+            for (int i = 0; i < iMax; i++)
+            {
+                if (i % 10 != 9 && i % 10 != 0)
+                {
+                    for (int j = 0; j < jMax; j++)
+                    {
+                        if (j % 10 != 9 && j % 10 != 0)
+                        {
+                            edgeCutBigPicture[(x, y)] = bigPicture[(j, i)];
+                            x ++;
+                        }
+                    }
+                    x = 0;
+                    y ++;
+                }
+            }
+
+            PrintDictToFile(edgeCutBigPicture, 100);
+
+            iMax = edgeCutBigPicture.Select(c => c.Key.y).Max();
+
+            var seaMonsterTop    = new Regex("                  # ".Replace(' ', '.'));
+            var seaMonsterMiddle = new Regex("#    ##    ##    ###".Replace(' ', '.'));
+            var seaMonsterBottom = new Regex(" #  #  #  #  #  #   ".Replace(' ', '.'));
+            var seaMonsterCount = 0;
+
+            foreach(var operation in operations)
+            {
+                var edgeCutBigPicture2 = Rotate(edgeCutBigPicture, operation, 'x');
+                PrintDictToFile(edgeCutBigPicture2, 100);
+
+                // scan the middle rows for the middle of the seamonster, note index
+                // scan adjacent rows at that index for those bits
+                // add one to count if there is one
+                for(int i = 1; i < iMax - 1; i++)
+                {
+                    var prevLine = new string(edgeCutBigPicture2.Where(e => e.Key.y == i-1).Select(e => e.Value).ToArray());
+                    var line = new string(edgeCutBigPicture2.Where(e => e.Key.y == i).Select(e => e.Value).ToArray());
+                    var nextLine = new string(edgeCutBigPicture2.Where(e => e.Key.y == i+1).Select(e => e.Value).ToArray());
+
+                    // regex match the middle
+                    var matches = seaMonsterMiddle.Matches(line);
+                    Match prevMatch = null;
+                    foreach(Match match in matches)
+                    {
+                        if (prevMatch != null && match.Index < prevMatch.Index + prevMatch.Length){
+                            // just in case of weird overlaps
+                            continue;
+                        }
+
+                        if (seaMonsterTop.Match(prevLine.Substring(match.Index, match.Length)).Success &&
+                            seaMonsterBottom.Match(nextLine.Substring(match.Index, match.Length)).Success)
+                            {
+                                prevMatch = match;
+                                seaMonsterCount ++;
+                            }
+                    }
+                }
+
+            }
+            var seaMonsterHashes = 15 * seaMonsterCount;
+            var totalHashes = edgeCutBigPicture.Count(c => c.Value == '#');
+
+            var ans = totalHashes - seaMonsterHashes;
+
+            Console.WriteLine(ans);
         }
 
-        public static void PrintDict(IDictionary<(int x, int y), char> dict){
+        public static void PrintDict(IDictionary<(int x, int y), char> dict, int gapsOn = 10){
             var maxX = dict.Keys.Select(k => k.x).Max();
             var maxY = dict.Keys.Select(k => k.y).Max();
 
             for (int i = 0; i <= maxY; i++)
             {
-                if (i % 10 == 0)
+                if (i % gapsOn == 0)
                 {
                     Console.WriteLine();
                 }
                 for (int j = 0; j <= maxX; j++)
                 {
-                    if (j % 10 == 0){
+                    if (j % gapsOn == 0){
                         Console.Write("  ");
                     }
                     Console.Write($"{dict[(j, i)]} ");
@@ -436,11 +639,39 @@ namespace Day_20
             }
         }
 
+        public static void PrintDictToFile(IDictionary<(int x, int y), char> dict, int gapsOn = 10){
+            var maxX = dict.Keys.Select(k => k.x).Max();
+            var maxY = dict.Keys.Select(k => k.y).Max();
+            
+            var sb = new StringBuilder();
+
+            for (int i = 0; i <= maxY; i++)
+            {
+                if (i % gapsOn == 0)
+                {
+                    sb.Append(Environment.NewLine);
+                }
+                for (int j = 0; j <= maxX; j++)
+                {
+                    if (j % gapsOn == 0){
+                        sb.Append("  ");
+                    }
+                    sb.Append($"{dict[(j, i)]} ");
+                }
+                sb.Append(Environment.NewLine);
+            }
+
+            File.WriteAllText("./output.txt", sb.ToString());
+        }
+
         public static IDictionary<(int x, int y), char> Rotate(IDictionary<(int x, int y), char> original, D4 op, char reflectionAxis)
         {
             if (op.Value == D4_element.E){
                 return original;
             }
+
+            var maxX = original.Select(c => c.Key.x).Max();
+            var maxY = original.Select(c => c.Key.y).Max();
 
             var d4str = op.ToString();
 
@@ -449,11 +680,11 @@ namespace Day_20
             
             while(d4str.Any() && d4str[0] == 'R')
             {
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i <= maxY; i++)
                 {
-                    for (int j = 9; j >= 0; j--)
+                    for (int j = maxX; j >= 0; j--)
                     {
-                        memDict[(i, 9-j)] = newDict[(j, i)];
+                        memDict[(i, maxX - j)] = newDict[(j, i)];
                     }
                 }
 
@@ -464,21 +695,21 @@ namespace Day_20
             if (d4str.Equals("F")){
                 if (reflectionAxis == 'x')
                 {
-                    for(int i = 0; i < 10; i++)
+                    for(int i = 0; i <= maxY; i++)
                     {
-                        for(int j = 0; j < 10; j++)
+                        for(int j = 0; j <= maxX; j++)
                         {
-                            memDict[(i, j)] = newDict[(i, 9-j)];
+                            memDict[(i, j)] = newDict[(i, maxX -j)];
                         }
                     }
                 }
                 else if (reflectionAxis == 'y')
                 {
-                    for(int i = 0; i < 10; i++)
+                    for(int i = 0; i <= maxY; i++)
                     {
-                        for(int j = 0; j < 10; j++)
+                        for(int j = 0; j <= maxX; j++)
                         {
-                            memDict[(i, j)] = newDict[(9-i, j)];
+                            memDict[(i, j)] = newDict[(maxY - i, j)];
                         }
                     }
                 }
