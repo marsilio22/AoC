5393555bc42909a493473cc9b987a3d8011af3a4
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2020-12-23 09:32:53 +0000

    2020 - Day 23 - neat

--- 2020/Day 23/Program.cs
+++ 2020/Day 23/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Day_23
@@ -8,147 +9,94 @@ namespace Day_23
     {
         static void Main(string[] args)
         {
-            var input = "135468729".Select(c => int.Parse(c.ToString())).ToList();
+            List<int> input = "135468729".Select(c => int.Parse(c.ToString())).ToList();
+            
+            // uncomment to do example instead
             //input = "389125467".Select(c => int.Parse(c.ToString())).ToList();
 
-            LinkedList<int> cups = new LinkedList<int>();
+            IDictionary<int, int> part1 = ShuffleCups(input, 100, input.Max());
+            string ans1 = string.Empty;
 
-            foreach(var c in input)
+            int num = part1[1];
+            Console.Write("Part 1: ");
+            while(num != 1)
             {
-                cups.AddLast(c);
+                Console.Write(num);
+                num = part1[num];
             }
-
-            var currentcup = cups.Find(cups.First()); 
-
-            // part 1
-            // for (int i = 0; i < 100; i ++)
-            // {
-            //     var cup = cups.Find(currentcup.Value);
-
-            //     var moving = new List<int>();
-            //     cup = cup.Next;
-            //     for (int k = 0; k < 3; k ++)
-            //     {
-            //         moving.Add(cup.Value);
-            //         cup = cup.Next;
-            //         if (cup == null)
-            //         {
-            //             cup = cups.Find(cups.First());
-            //             cups.RemoveLast();
-            //         }
-            //         else{
-            //             cups.Remove(cup.Previous);
-            //         }
-            //     }
-
-            //     var dest = cups.Find(currentcup.Value - 1);
-            //     var j = 2;
-            //     while (dest == null)
-            //     {
-            //         if (currentcup.Value - j < 1)
-            //         {
-            //             j = currentcup.Value - cups.Max();
-            //         }
-            //         dest = cups.Find(currentcup.Value - j);
-            //         j++;
-            //     }
-
-            //     for (j = 0; j < 3; j++)
-            //     {
-            //         cups.AddAfter(dest, moving[j]);
-            //         dest = dest.Next;
-            //     }
-
-            //     currentcup = currentcup.Next;
-            //     if (currentcup == null)
-            //     {
-            //         currentcup = cups.Find(cups.First());
-            //     }
-            // }
-
-            // currentcup = cups.Find(1).Next;
-
-            // for (int i = 0; i < cups.Count - 1; i ++)
-            // {
-            //     if (currentcup == null)
-            //     {
-            //         currentcup = cups.Find(cups.First());
-            //     }
-            //     Console.Write(currentcup.Value);
-            //     currentcup = currentcup.Next;
-            // }
-            Console.WriteLine();
             Console.WriteLine();
 
+            IDictionary<int, int> part2 = ShuffleCups(input, 10_000_000, 1_000_000);
+            long ans2 = (long)part2[1] * (long)part2[part2[1]];
+
+            Console.WriteLine($"Part 2: {ans2}");
+        }
+
+        public static IDictionary<int, int> ShuffleCups(List<int> input, int turns, int biggestCup)
+        {
             // each key contains the next value
-            var cups2 = new Dictionary<int, int>();
+            Dictionary<int, int> cups = new Dictionary<int, int>();
             for (int i = 0; i < input.Count() - 1; i++)
             {
-                cups2.Add(input[i], input[i+1]);
+                cups.Add(input[i], input[i+1]);
             }
 
-            // cups2.Add(input.Last(), cups2.First().Key);
-            cups2.Add(input.Last(), input.Max() + 1);
-
-            for(int i = cups.Max() + 1; i < 1_000_000; i++)
+            for(int i = cups.Keys.Max() + 1; i < biggestCup; i++)
             {
-                cups2.Add(i, i+1);
+                cups.Add(i, i+1);
             }
 
-            cups2.Add(1_000_000, cups2.First().Key);
+            cups.Add(biggestCup, cups.First().Key);
 
-            var currentCup = cups2.First(); 
+            KeyValuePair<int, int> currentCup = cups.First();
+            Stopwatch sw = new Stopwatch();
 
-            for (int i = 0; i < 10_000_000; i ++ )
+            sw.Start();
+            for (int i = 0; i < turns; i ++ )
             {
-                var nextCup = new KeyValuePair<int, int>(currentCup.Value, cups2[currentCup.Value]);
+                KeyValuePair<int, int> nextCup = new KeyValuePair<int, int>(currentCup.Value, cups[currentCup.Value]);
 
                 // the first cup we move is the one immediately after the current one
                 // i.e. the value of the current cup
-                var movingStart = nextCup.Key;
+                int movingStart = nextCup.Key;
 
-                // the last cup we move is the third one after that
-                var movingEnd = cups2[nextCup.Value];
+                // the last cup we move is the one after the one after that
+                int movingEnd = cups[nextCup.Value];
 
-                // remove them by setting the current cup's value, to the value of
-                // the cup after the end of the section we're moving
-                cups2[currentCup.Key] = cups2[movingEnd];
+                // "remove" them by setting the current cup's value to the value of
+                // the cup after the end of the section we're moving (so that the cup after
+                // the current cup is now the cup after the last moving cup)
+                cups[currentCup.Key] = cups[movingEnd];
 
                 // find where we're moving them to;
-                var destVal = currentCup.Key - 1;
+                int destVal = currentCup.Key - 1;
                 if (destVal < 1)
                 {
-                    destVal = cups2.Keys.Max();
+                    destVal = cups.Keys.Max();
                 }
 
                 while (destVal == movingStart ||
-                    destVal == cups2[movingStart] ||
+                    destVal == cups[movingStart] ||
                     destVal == movingEnd)
                 {       
                     destVal--;
                     if (destVal < 1)
                     {
-                        destVal = cups2.Keys.Max();
+                        destVal = cups.Keys.Max();
                     }
                 }
 
-                var afterDest = cups2[destVal];
-                cups2[destVal] = movingStart;
-                cups2[movingEnd] = afterDest;
+                int afterDest = cups[destVal];
+                cups[destVal] = movingStart;
+                cups[movingEnd] = afterDest;
 
-                currentCup = new KeyValuePair<int, int>(cups2[currentCup.Key], cups2[cups2[currentCup.Key]]);
-
-                // if (i % 10000 == 0)
-                // {
-                //     Console.WriteLine($"{DateTime.Now}: {i/10000}% complete");
-                // }
+                currentCup = new KeyValuePair<int, int>(cups[currentCup.Key], cups[cups[currentCup.Key]]);
             }
+            sw.Stop();
 
-            var starCup = cups.Find(1).Next;
+            Console.WriteLine($"Shuffling {biggestCup} cups for {turns} turns took {sw.Elapsed}");
 
-            long ans = (long)starCup.Next.Value * (long)starCup.Next.Next.Value;
-
-            Console.WriteLine(ans);
+            return cups;
         }
     }
 }
