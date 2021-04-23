f64749e73e392ba8321414342ccac5fddd3e2f42
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2021-04-23 22:28:49 +0100

    2017 - Days 20 and 21 part 1
    Need to fix an off by one error for day 21 before I will be able to do part 2. Some tidying is probably also required because I've really made a meal of it :D

---
+++ 2017/Day 20/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("./input.txt");
+
+            List<Point> points = new List<Point>(1000);
+
+            var count = 0;
+
+            foreach (var line in input)
+            {
+                var splitLine = line.Split(", ");
+                
+                var pos = ParseToCoord(splitLine[0]);
+                var vel = ParseToCoord(splitLine[1]);
+                var acc = ParseToCoord(splitLine[2]);
+
+                points.Add(new Point{Id = count, Position = pos, Velocity = vel, Acceleration = acc});
+                count++;
+            }
+
+            for(int i = 0; i < 10000; i++)
+            {
+                foreach(var point in points)
+                {
+                    point.Tick();
+                }
+            }
+
+            ICollection<(int id, long distance)> distances = points.Select(p => (p.Id, p.CalculateDistanceFromOrigin())).ToList();
+
+            var minDistance = distances.Single(e => e.distance == distances.Min(d => d.distance));
+
+            Console.WriteLine(minDistance.id);
+
+            // reset for part 2, todo split into method 
+            points = new List<Point>(1000);
+            count = 0;
+
+            foreach (var line in input)
+            {
+                var splitLine = line.Split(", ");
+                
+                var pos = ParseToCoord(splitLine[0]);
+                var vel = ParseToCoord(splitLine[1]);
+                var acc = ParseToCoord(splitLine[2]);
+
+                points.Add(new Point{Id = count, Position = pos, Velocity = vel, Acceleration = acc});
+                count++;
+            }
+
+            for(int i = 0; i < 10000; i++)
+            {
+                foreach(var point in points)
+                {
+                    point.Tick();
+                }
+
+                // distances = points.Select(p => (p.Id, p.CalculatePositionSum())).ToList();
+
+                // if (distances.Any(d => distances.Count(e => e.id != d.id && e.distance == d.distance) > 0))
+                // {
+                    
+                // }
+
+                List<Point> pointsToRemove = new List<Point>();
+
+                foreach(var point in points)
+                {
+                    if (!pointsToRemove.Contains(point) && points.Count(p => p.Id != point.Id && p.Position == point.Position) > 0)
+                    {
+                        pointsToRemove.AddRange(points.Where(p => p.Position == point.Position));
+                    }
+                }
+
+                points.RemoveAll(p => pointsToRemove.Contains(p));
+            }
+
+
+            Console.WriteLine(points.Count());
+        }
+
+        public static (long x, long y, long z) ParseToCoord(string input)
+        {
+            var thingList = input.Split('<')[1].Split('>')[0].Split(',').Select(s => long.Parse(s)).ToList();
+            return (thingList[0], thingList[1], thingList[2]);
+        }
+    }
+
+    public class Point
+    {
+        public int Id { get; set; }
+        public (long x, long y, long z) Position {get;set;}
+        public (long x, long y, long z) Velocity {get;set;}
+        public (long x, long y, long z) Acceleration {get;set;}
+
+        public void Tick()
+        {
+            this.Velocity = (
+                this.Velocity.x + this.Acceleration.x, 
+                this.Velocity.y + this.Acceleration.y, 
+                this.Velocity.z + this.Acceleration.z
+            );
+            
+            this.Position = (
+                this.Position.x + this.Velocity.x,
+                this.Position.y + this.Velocity.y,
+                this.Position.z + this.Velocity.z
+            );
+        }
+
+        public long CalculateDistanceFromOrigin()
+        {
+            return Math.Abs(Position.x) + Math.Abs(Position.y) + Math.Abs(Position.z);
+        }
+
+        public long CalculatePositionSum()
+        {
+            return Position.x + Position.y + Position.z;
+        }
+    }    
+}
---
+++ 2017/Day 21/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("./input.txt");
+
+            var iterations = 18;
+
+            var rules = new Dictionary<string, string>();
+
+            foreach(var line in lines)
+            {
+                var splitLine = line.Split(" => ");
+                rules.Add(splitLine[0], splitLine[1]);
+            }
+
+            var startingImage = new Dictionary<(int x, int y), char>{
+                {(0, 0), '.'}, {(1, 0), '#'}, {(2, 0), '.'},
+                {(0, 1), '.'}, {(1, 1), '.'}, {(2, 1), '#'},
+                {(0, 2), '#'}, {(1, 2), '#'}, {(2, 2), '#'}
+            };
+
+            var image = ".#./..#/###";
+            for(int i = 0; i < iterations; i++)
+            {
+                if (image.EndsWith('/'))
+                {
+                    image = image.Substring(0, image.Length - 1);
+                }
+
+                var imageLines = image.Split('/');
+                var size = Math.Sqrt(image.Count(i => i != '/'));
+                image = string.Empty;
+
+                if (size % 2 == 0)
+                {
+                    var j = 0;
+                    // while there are still rows to process
+                    while(j < imageLines.Count())
+                    {
+                        // take the next two rows
+                        (string line1, string line2) linesOfInterest = (imageLines[j], imageLines[j+1]);
+                        (string line1, string line2, string line3) next = (string.Empty, string.Empty, string.Empty);
+
+                        var k = 0;
+                        // go through two columns at a time
+                        while(k <linesOfInterest.line1.Length)
+                        {
+                            char c1 = linesOfInterest.line1[k], c2 = linesOfInterest.line1[k+1], 
+                                c3 = linesOfInterest.line2[k], c4 = linesOfInterest.line2[k+1];
+                            // now rotate & flip until we find the rule in the dict
+                            next = RotateFlipTransform2x2(c1, c2, c3, c4, next, rules);
+
+                            k += 2;
+                        }
+
+                        image += string.Join('/', next.line1, next.line2, next.line3) + '/';
+
+                        j += 2;
+                    }
+                }
+                else if (size % 3 == 0)
+                {
+                    var j = 0;
+                    // while there are still rows to process
+                    while(j < imageLines.Count())
+                    {
+                        // take the next two rows
+                        (string line1, string line2, string line3) linesOfInterest = (imageLines[j], imageLines[j+1], imageLines[j+2]);
+                        (string line1, string line2, string line3, string line4) next = (string.Empty, string.Empty, string.Empty, string.Empty);
+
+                        var k = 0;
+                        // go through two columns at a time
+                        while(k < linesOfInterest.line1.Length)
+                        {
+                            char c1 = linesOfInterest.line1[k], c2 = linesOfInterest.line1[k+1], c3 = linesOfInterest.line1[k+2], 
+                                c4 = linesOfInterest.line2[k], c5 = linesOfInterest.line2[k+1], c6 = linesOfInterest.line2[k+2],
+                                c7 = linesOfInterest.line3[k], c8 = linesOfInterest.line3[k+1], c9 = linesOfInterest.line3[k+2];
+                            // now rotate & flip until we find the rule in the dict
+                            next = RotateFlipTransform3x3(c1, c2, c3, c4, c5, c6, c7, c8, c9, next, rules);
+
+                            k += 3;
+                        }
+
+                        image += string.Join('/', next.line1, next.line2, next.line3, next.line4) + '/';
+
+                        j += 3;
+                    }
+                }
+            }
+
+            // 132 too low
+            // 133 is correct. Why am I off by 1?!
+
+            // 2111399 too low
+            // 2111400 too low - gonna have to fix this off by one somewhere I think.
+            Console.WriteLine(image.Count(c => c == '#'));
+        }
+
+        
+        public static (string line1, string line2, string line3) RotateFlipTransform2x2(char c1, char c2, char c3, char c4, (string line1, string line2, string line3) next, IDictionary<string, string> rules)
+        {
+            char hold;
+            for (int t = 0; t < 2; t++)
+            {
+                for (int q = 0; q < 4; q ++)
+                {
+                    if (rules.TryGetValue(c1.ToString()+c2+'/'+c3+c4, out string newLines))
+                    {
+                        // replace with the three rows from the rules
+                        var splitNewLines = newLines.Split('/');
+                        return (next.line1 + splitNewLines[0], next.line2 + splitNewLines[1], next.line3 + splitNewLines[2]);
+                    }
+
+                    //rotate
+                    hold = c1;
+                    c1 = c2;
+                    c2 = c4;
+                    c4 = c3;
+                    c3 = hold;
+                }
+
+                // flip
+                hold = c1;
+                c1 = c2;
+                c2 = hold;
+                hold = c3;
+                c3 = c4;
+                c4 = hold;
+            }
+
+            throw new Exception("oh noes");
+        }
+
+        public static (string line1, string line2, string line3, string line4) RotateFlipTransform3x3(char c1, char c2, char c3, char c4, char c5, char c6, char c7, char c8, char c9, (string line1, string line2, string line3, string line4) next, IDictionary<string, string> rules)
+        {
+            char hold;
+            for(int t = 0; t < 2; t++)
+            {
+                for(int q = 0; q < 4; q++)
+                {
+                    if (rules.TryGetValue(c1.ToString()+c2+c3+'/'+c4+c5+c6+'/'+c7+c8+c9, out string newLines))
+                    {
+                        // replace with the three rows from the rules
+                        var splitNewLines = newLines.Split('/');
+                        return (next.line1 + splitNewLines[0], next.line2 + splitNewLines[1], next.line3 + splitNewLines[2], next.line4 + splitNewLines[3]);
+                    }
+                    //rotate
+                    hold = c1;
+                    c1 = c3;
+                    c2 = c6;
+                    c3 = c9;
+                    c6 = c8;
+                    c9 = c7;
+                    c8 = c4;
+                    c7 = hold;
+                    c4 = c2;
+                }
+
+                // flip
+                hold = c1;
+                c1 = c3;
+                c3 = hold;
+                hold = c4;
+                c4 = c6;
+                c6 = hold;
+                hold = c7;
+                c7 = c9;
+                c9 = hold;
+            }
+
+            throw new Exception("oh noes");
+        }
+    }
+
+}
