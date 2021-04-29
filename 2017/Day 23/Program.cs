9eb557b93e7e6608ad2786cfce1622497ff5b78c
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2021-04-29 22:17:05 +0100

    Day 23 - Part 2

--- 2017/Day 23/Program.cs
+++ 2017/Day 23/Program.cs
@@ -13,9 +13,34 @@ namespace Day_23
         {
             var lines = File.ReadAllLines("./input.txt").ToList();
             
-            var runner = new Runner(lines, null, 0);
+            var runner = new Runner(lines);
 
             runner.Run();
+
+            // 267848112 too high
+            // 133924056 too high
+            // 6264 ( = 106500/17) too high
+            // 1000 incorrect
+            // 5194 incorrect
+            // 2326 incorrect
+            // 4652 incorrect
+
+            var b = 106500;
+            var h = 0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                var x = (b + (17 * i));
+                for (var d = 2; d <= x/2; d++)
+                {
+                    if (x % d == 0)
+                    {
+                        h++;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(h);
         }
     }
 
@@ -23,22 +48,14 @@ namespace Day_23
     {
         public List<string> Program { get; set; }
 
-        public Runner OtherRunner { get; set; }
-
-        public BlockingCollection<long> Queue { get; set; } = new BlockingCollection<long>();
-
-        public int Name { get; set; }
-
-        public Runner(List<string> program, Runner otherRunner, int name)
+        public Runner(List<string> program)
         {
             this.Program = program;
-            this.OtherRunner = otherRunner;
-            this.Name = name;
         }
 
         public void Run()
         {
-            var registers = new Dictionary<string, long>{ {"a", 1} };
+            var registers = new Dictionary<string, long>{ {"a", 0} };
             long numberOfMuls = 0;
 
             for (long i = 0; i < Program.Count() && i >= 0; i++)
@@ -68,7 +85,7 @@ namespace Day_23
                     case "mul":
                         registers[splitLine[1]] = val1 * val2;
                         numberOfMuls++;
-                        // Console.WriteLine(numberOfMuls);
+                        Console.WriteLine(numberOfMuls);
                         break;
                     case "jnz":
                         if (val1 != 0)
