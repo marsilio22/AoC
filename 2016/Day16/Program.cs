4f90a4f1bfa1dcf63a8355e82234ba6e6537ed5d
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2021-05-25 22:05:43 +0100

    2016 - Other days structure
    Solutions TBD

---
+++ 2016/Day16/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines = File.ReadAllLines("./input.txt").ToList();
+        }
+    }
+}
