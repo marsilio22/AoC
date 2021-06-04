f435ade67eed56078ef71d5ecdcb434cc96a50f5
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2021-06-04 18:12:51 +0100

    2016 Day 6

--- 2016/Day6/Program.cs
+++ 2016/Day6/Program.cs
@@ -10,6 +10,28 @@ namespace Day6
         static void Main(string[] args)
         {
             List<string> lines = File.ReadAllLines("./input.txt").ToList();
+
+            var messageP1 = "";
+            var messageP2 = "";
+
+            for (int i = 0; i < 8; i++)
+            {
+                var chars = new Dictionary<char, int>();
+                foreach(var line in lines)
+                {
+                    var c = line[i];
+
+                    chars[c] = chars.ContainsKey(c) ? chars[c]+1 : 1;
+                }
+
+                var orderedValues = chars.OrderByDescending(c => c.Value).Select(c => c.Key).ToList();
+
+                messageP1 += orderedValues.First();
+                messageP2 += orderedValues.Last();
+            }
+
+            Console.WriteLine(messageP1);
+            Console.WriteLine(messageP2);
         }
     }
 }
