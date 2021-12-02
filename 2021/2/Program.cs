b21a3e4297bd001c494f08a30e6b06cdde5e4a9f
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2021-12-02 07:43:59 +0000

    AoC 2021 - Day 2
    Not sure VS Code can handle the new console syntax :D

---
+++ 2021/2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+var lines = File.ReadAllLines("./input.txt");
+
+ICollection<(string dir, int value)> parsed = lines.Select(l => l.Split(" ")).Select(l => (l[0], int.Parse(l[1]))).ToList();
+
+var grouped = parsed.GroupBy(p => p.dir);
+
+IDictionary<string, int> values = new Dictionary<string, int>();
+
+foreach(var group in grouped) 
+{
+    values[group.Key] = group.Sum(g => g.value);
+}
+
+var answer = values["forward"] * (values["down"]- values["up"]);
+
+Console.WriteLine(answer);
+
+var aim = 0;
+var horizontal = 0;
+var depth = 0;
+foreach(var line in parsed)
+{
+    switch (line.dir) {
+        case "forward":
+            horizontal += line.value;
+            depth += line.value * aim;
+            break;
+        case "up":
+            aim -= line.value;
+            break;
+        case "down":
+            aim += line.value;
+            break;
+        default:
+            throw new Exception();
+            break;
+    }
+}
+
+answer = horizontal * depth;
+
+Console.WriteLine(answer);
