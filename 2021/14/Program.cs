a722e999f0bdfb565c3b16e10b1b2d13989ff252
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2021-12-15 08:09:35 +0000

    AoC 2021 - Day 14 tidy
    Still could be less but cba

--- 2021/14/Program.cs
+++ 2021/14/Program.cs
@@ -16,163 +16,63 @@ foreach(var line in lines)
     instructions[key] = splitLine[1][0];
 }
 
-var chain = new LinkedList<char>();
-
-foreach(var character in starter)
+for (int j = 10;j <= 40; j+=30)
 {
-    chain.AddLast(character);
-}
-
-Console.WriteLine(string.Join("", chain.ToList()));
-
-for (int i = 0; i < 10; i++)
-{
-    var copy = chain.ToList();
-
-    var current = chain.Find(chain.First());
-
-    for(int j = 0; j < copy.Count() - 1; j++)
+    var dictionaryOfResults = instructions.ToDictionary(i => i.Key, _ => (long)0);
+    for (int k = 0; k < starter.Length - 1; k ++)
     {
-        var newCharacter = instructions[(copy[j], copy[j+1])];
-
-        chain.AddAfter(current, newCharacter);
-
-        current = current.Next.Next;
+        dictionaryOfResults.TryGetValue((starter[k], starter[k+1]), out var value);
+        value += 1;
+        dictionaryOfResults[(starter[k], starter[k+1])] = value;
     }
-    
-    Console.WriteLine(string.Join("", chain.ToList()));
-}
 
-var ans = chain.ToList().GroupBy(c => c).ToDictionary(c => c.Key, c => (long)c.Count()).OrderBy(d => d.Value);
-
-Console.WriteLine(Math.Abs(ans.First().Value - ans.Last().Value));
-
-
-// part2
-
-// chain = new LinkedList<char>();
-
-// foreach(var character in starter)
-// {
-//     chain.AddLast(character);
-// }
-
-// // for every pair in the chain, figure out what is produced after a few steps and when you can know everything about what will happen in 100 steps
-
-// foreach(var inst in instructions)
-// {
-//     var testStart = new LinkedList<char>();
-    
-//     testStart.AddLast(inst.Key.L);
-//     testStart.AddLast(inst.Key.R);
-
-//     int count = 0;
-
-//     var previouslySeenFirstEntries = new List<char>{inst.Key.R};
-
-//     var previousStates = new Dictionary<int, string>();
-//     previousStates.Add(count, inst.Key.L.ToString() + inst.Key.R);
-
-//     while(true)
-//     {
-//         var copy = testStart.ToList();
-
-//         var current = testStart.Find(testStart.First());
-
-//         for(int j = 0; j < copy.Count() - 1; j++)
-//         {
-//             var newCharacter = instructions[(copy[j], copy[j+1])];
-
-//             testStart.AddAfter(current, newCharacter);
-
-//             current = current.Next.Next;
-//         }
-
-//         count++;
-
-//         previousStates.Add(count, string.Join("", testStart.ToList()));
-
-//         if (previouslySeenFirstEntries.Contains(testStart.ToList()[1]))
-//         {
-//             if (previouslySeenFirstEntries.First() == testStart.ToList()[1])
-//             {
-//                 Console.WriteLine("head");
-//             }
-//             else
-//             {
-//                 Console.WriteLine("tail only");
-//             }
-//             break;
-//         }
-//         else
-//         {
-//             previouslySeenFirstEntries.Add(testStart.ToList()[1]);
-//         }
-        
-//     }
-
-//     var loopPair = testStart.ToList()[0].ToString() + testStart.ToList()[1];
-
-//     // find first occurrence of loop pair in previous states
-//     // determine remaining steps to 100
-//     // do I need to do recursion instead??
-
-//     Console.WriteLine(inst.Key.L.ToString() + inst.Key.R + " -> " + string.Join("", testStart.ToList()) + $" after {count} steps");
-// }
-
-
-var dictionaryOfResults = instructions.ToDictionary(i => i.Key, _ => (long)0);
-for (int k = 0; k < starter.Length - 1; k ++)
-{
-    dictionaryOfResults.TryGetValue((starter[k], starter[k+1]), out var value);
-    value += 1;
-    dictionaryOfResults[(starter[k], starter[k+1])] = value;
-}
-
-for( int i = 0; i < 10; i++)
-{
-    var copy = dictionaryOfResults.ToList();
-
-    foreach(var resi in copy)
+    for( int i = 0; i < j; i++)
     {
-        if (resi.Value == 0)
+        var copy = dictionaryOfResults.ToList();
+
+        foreach(var resi in copy)
         {
-            continue;
+            if (resi.Value == 0)
+            {
+                continue;
+            }
+
+            var insertChar = instructions[resi.Key];
+            
+            var value = dictionaryOfResults[(resi.Key.L, insertChar)];
+            value += resi.Value;
+            dictionaryOfResults[(resi.Key.L, insertChar)] = value;
+
+            value = dictionaryOfResults[(insertChar, resi.Key.R)];
+            value += resi.Value;
+            dictionaryOfResults[(insertChar, resi.Key.R)] = value;
+
+            value = dictionaryOfResults[resi.Key];
+            value -= resi.Value;
+            dictionaryOfResults[resi.Key] = value;
         }
+}
 
-        var insertChar = instructions[resi.Key];
-        
-        var value = dictionaryOfResults[(resi.Key.L, insertChar)];
-        value += resi.Value;
-        dictionaryOfResults[(resi.Key.L, insertChar)] = value;
+    var alphabet = new Dictionary<char, long>();
 
-        value = dictionaryOfResults[(insertChar, resi.Key.R)];
-        value += resi.Value;
-        dictionaryOfResults[(insertChar, resi.Key.R)] = value;
+    foreach (var entry in dictionaryOfResults)
+    {
+        alphabet.TryGetValue(entry.Key.L, out var value);
+        value += entry.Value;
+        alphabet[entry.Key.L] = value;
 
-        value = dictionaryOfResults[resi.Key];
-        value -= resi.Value;
-        dictionaryOfResults[resi.Key] = value;
+        alphabet.TryGetValue(entry.Key.R, out value);
+        value += entry.Value;
+        alphabet[entry.Key.R] = value;
     }
+
+    // the first and last characters only appear in one pair each, so pretend they don't
+    var val = alphabet[starter.Last()];
+    alphabet[starter.Last()] = val + 1;
+    val = alphabet[starter.First()];
+    alphabet[starter.First()] = val + 1;
+
+    var alphabetCop = alphabet.OrderBy(a => a.Value).ToList();
+
+    Console.WriteLine(Math.Abs(alphabetCop.First().Value / 2 - alphabetCop.Last().Value / 2));
 }
-
-var alphabet = new Dictionary<char, long>();
-
-foreach (var entry in dictionaryOfResults)
-{
-    alphabet.TryGetValue(entry.Key.L, out var value);
-    value += entry.Value;
-    alphabet[entry.Key.L] = value;
-
-    alphabet.TryGetValue(entry.Key.R, out value);
-    value += entry.Value;
-    alphabet[entry.Key.R] = value;
-}
-
-var val = alphabet[starter.Last()];
-alphabet[starter.Last()] = val + 1; // why?
-
-var alphabetCop = alphabet.OrderBy(a => a.Value).ToList();
-
-Console.WriteLine(Math.Abs(alphabetCop.First().Value / 2 - alphabetCop.Last().Value / 2));
-// 3390034818250 too high
