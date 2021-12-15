864a4c8367fe850e0500e8dfaf57f3fb14fb132b
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2021-12-15 17:37:11 +0000

    AoC 2021 - Day 15 faster now

--- 2021/15/Program.cs
+++ 2021/15/Program.cs
@@ -1,62 +1,67 @@
-﻿var lines = File.ReadAllLines("./input.txt").ToList();
-
-var map = new Dictionary<(int x, int y), Node>();
-
-for (int c = 0; c < 5; c ++)
+﻿for (int tesselationWidth = 1; tesselationWidth <= 5; tesselationWidth += 4)
 {
-    for(int d = 0; d < 5; d ++)
+    var lines = File.ReadAllLines("./input.txt").ToList();
+
+    var map = new Dictionary<(int x, int y), Node>();
+
+    for (int c = 0; c < tesselationWidth; c ++)
     {
-        for(int j = 0; j < lines.Count(); j++)
+        for(int d = 0; d < tesselationWidth; d ++)
         {
-            var line = lines[j];
-            for (int i = 0; i < line.Length; i++)
+            for(int j = 0; j < lines.Count(); j++)
             {
-                map[(i + d * line.Length, j + c * line.Length)] = new Node((i + d * line.Length, j + c * lines.Count), int.Parse(line[i].ToString() )+ c + d, 5 * lines.Count - 1);
+                var line = lines[j];
+                for (int i = 0; i < line.Length; i++)
+                {
+                    map[(i + d * line.Length, j + c * line.Length)] = new Node((i + d * line.Length, j + c * lines.Count), int.Parse(line[i].ToString() )+ c + d, tesselationWidth * lines.Count - 1);
+                }
             }
         }
     }
+
+    // draw
+    // var maxX = map.MaxBy(m => m.Key.x).Key.x;
+    // var maxY = map.MaxBy(m => m.Key.y).Key.y;
+
+    // for(int j = 0; j <= maxY; j++)
+    // {
+    //     for (int i = 0; i <= maxX; i++)
+    //     {
+    //         Console.Write(map[(i,j)].Risk);
+    //     }
+    //     Console.WriteLine();
+    // }
+
+    (int x, int y) current = (0, 0);
+    (int x, int y) end = map.Last().Key;
+
+    var adjacents = new Dictionary<(int x, int y), int>();
+
+    foreach(var adj in map[current].Adjacents)
+    {
+        adjacents.Add(adj, map[adj].Risk);
+    }
+
+    map[current].Distance = 0;
+
+    while(adjacents.Any() && map[end].Distance == int.MaxValue)
+    {
+        var next = adjacents.MinBy(a => a.Value);
+
+        map[next.Key].Distance = next.Value;
+
+        adjacents.Remove(next.Key);
+
+        var newAdjacents = map[next.Key].Adjacents.Where(a => map[a].Distance > next.Value);
+        foreach(var adj in newAdjacents)
+        {
+            adjacents[adj] = next.Value + map[adj].Risk;
+        }
+    }
+
+    Console.WriteLine(map[end].Distance);
+
 }
-Console.WriteLine();
-
-// draw
-// var maxX = map.MaxBy(m => m.Key.x).Key.x;
-// var maxY = map.MaxBy(m => m.Key.y).Key.y;
-
-// for(int j = 0; j <= maxY; j++)
-// {
-//     for (int i = 0; i <= maxX; i++)
-//     {
-//         Console.Write(map[(i,j)].Risk);
-//     }
-//     Console.WriteLine();
-// }
-
-(int x, int y) current = (0, 0);
-(int x, int y) end = map.Last().Key;
-
-var adjacents = new List<(int x, int y)>(); // priorityqueue here instead?
-
-adjacents.AddRange(map[current].Adjacents);
-
-map[current].Distance = 0;
-
-while(adjacents.Any() && map[end].Distance == int.MaxValue)
-{
-    // this is probably inefficient eh?
-    var next = adjacents.MinBy(a => map[a].Risk + map[map[a].Adjacents.MinBy(b => map[b].Distance)].Distance); 
-
-    map[next].Distance = map[next].Risk + map[map[next].Adjacents.MinBy(a => map[a].Distance)].Distance;
-
-    adjacents.Remove(next);
-
-    adjacents.AddRange(map[next].Adjacents.Where(a => map[a].Distance > map[next].Distance));
-
-    adjacents = adjacents.Distinct().ToList();
-}
-
-Console.WriteLine(map[end].Distance);
-
-
 public class Node
 {
     public (int x, int y) Coord {get; set;}
