3fde938877e41c191b76b8ed6a24387eb84cf178
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2021-12-23 15:44:38 +0000

    AoC 2021 - Day 21

---
+++ 2021/21/Program.cs
@@ -0,0 +1,102 @@
+﻿// i'm choosing to confuse myself by zero indexing the board
+var board = new Dictionary<int, int>
+{
+    { 0, 1 },
+    { 1, 2},
+    { 2, 3},
+    { 3, 4},
+    { 4, 5},
+    { 5, 6},
+    { 6, 7},
+    { 7, 8},
+    { 8, 9},
+    { 9, 10}
+};
+
+var p1Space = 9;
+var p2Space = 8;
+
+var possibleRollsInOrder = new List<int>();
+
+for (int i = 1;possibleRollsInOrder.Count < 100; i = (i + 3) > 100 ? i-97 : i+3)
+{
+    var firstTerm = i;
+    var secondTerm = i+1 > 100? i -99 : i+1;
+    var thirdTerm = i+2 > 100 ? i - 98 : i+2;
+    possibleRollsInOrder.Add(firstTerm + secondTerm + thirdTerm);
+}
+
+var p1Score = 0;
+var p2Score = 0;
+
+var nextDiceRoll = 0;
+var diceRollCounter = 0;
+while (true)
+{
+    p1Space = (p1Space + possibleRollsInOrder[nextDiceRoll]) % 10;
+    p1Score += board[p1Space];
+    
+    nextDiceRoll ++;
+    nextDiceRoll %= 100;
+
+    diceRollCounter += 3;
+    
+    if (p1Score >= 1000)
+    {
+        break;
+    }
+
+    p2Space = (p2Space + possibleRollsInOrder[nextDiceRoll]) % 10;
+    p2Score += board[p2Space];
+    
+    nextDiceRoll ++;
+    nextDiceRoll %= 100;
+
+    diceRollCounter += 3;
+
+    if (p2Score >= 1000)
+    {
+        break;
+    }
+}
+
+Console.WriteLine((long)Math.Min(p2Score, p1Score) * diceRollCounter); //306027 too low
+
+
+// part 2
+
+// all of these happen on each player's turn
+var possibilities = new List<int>
+{
+    1 + 1 + 1,
+    1 + 1 + 2,
+    1 + 1 + 3,
+    1 + 2 + 1,
+    1 + 2 + 2,
+    1 + 2 + 3,
+    1 + 3 + 1,
+    1 + 3 + 2,
+    1 + 3 + 3,
+    2 + 1 + 1,
+    2 + 1 + 2,
+    2 + 1 + 3,
+    2 + 2 + 1,
+    2 + 2 + 2,
+    2 + 2 + 3,
+    2 + 3 + 1,
+    2 + 3 + 2,
+    2 + 3 + 3,
+    3 + 1 + 1,
+    3 + 1 + 2,
+    3 + 1 + 3,
+    3 + 2 + 1,
+    3 + 2 + 2,
+    3 + 2 + 3,
+    3 + 3 + 1,
+    3 + 3 + 2,
+    3 + 3 + 3
+};
+
+var groups = possibilities.GroupBy(p => p).Select(g => (g.Key, g.Count())).ToList();
+
+Console.WriteLine();
