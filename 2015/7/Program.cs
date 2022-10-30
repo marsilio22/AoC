51472f7f2da39c845436f322ff17c9fb150e557b
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2022-10-30 14:47:40 +0000

    2015 - Days 7 and 8
    regex fun

---
+++ 2015/7/Program.cs
@@ -0,0 +1,82 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+var dict = new Dictionary<string, int>();
+
+var part1 = GetA(lines, dict);
+
+dict = new Dictionary<string, int>();
+dict["b"] = part1;
+
+var part2 = GetA(lines, dict);
+
+Console.WriteLine(part1);
+Console.WriteLine(part2);
+
+static int GetA(string[] lines, Dictionary<string, int> dict)
+{
+    while (!dict.ContainsKey("a"))
+    {
+        foreach (var line in lines)
+        {
+            var split = line.Split(" -> ");
+            var instr = split[0];
+            var output = split[1];
+
+            if (dict.ContainsKey(output))
+            {
+                continue;
+            }
+
+            if (int.TryParse(instr, out var value) || dict.TryGetValue(instr, out value))
+            {
+                dict.Add(output, value);
+                continue;
+            }
+
+            var splitInstr = instr.Split(" ");
+
+            var firstExists = dict.TryGetValue(splitInstr.First(), out int first);
+            var secondExists = dict.TryGetValue(splitInstr.Last(), out int second);
+
+            if (!firstExists && int.TryParse(splitInstr.First(), out value))
+            {
+                firstExists = true;
+                first = value;
+            }
+            if (!secondExists && int.TryParse(splitInstr.Last(), out value))
+            {
+                secondExists = true;
+                second = value;
+            }
+
+            if (instr.Contains("AND") && firstExists && secondExists)
+            {
+                dict[output] = first & second;
+            }
+            else if (instr.Contains("OR") && firstExists && secondExists)
+            {
+                dict[output] = first | second;
+            }
+            else if (instr.Contains("LSHIFT") && firstExists)
+            {
+                var amount = int.Parse(splitInstr.Last());
+                dict[output] = first << amount;
+            }
+            else if (instr.Contains("RSHIFT") && firstExists)
+            {
+                var amount = int.Parse(splitInstr.Last());
+                dict[output] = first >> amount;
+            }
+            else if (instr.Contains("NOT") && secondExists)
+            {
+                dict[output] = ~second;
+            }
+
+        }
+
+        dict = dict.ToDictionary(d => d.Key, d => d.Value < 0 ? d.Value + 65536 : d.Value);
+    }
+
+    var result = dict["a"];
+    return result;
+}
---
+++ 2015/8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("./input.txt");
+
+int charDiff = 0;
+int extraEncodedChars = 0;
+
+// match \x27 etc, but DON'T match \\x27, but DO match \\\x27 :D
+Regex hexCharacterLookup = new Regex(@"(?=[^\\]\\x..){1}|\\\\\\x..");
+
+// escaped backslash \\
+Regex slashLookup = new Regex(@"\\\\");
+
+// escaped quote, not immediately followed by the end of the line 
+// (e.g. if the string ENDS with a backslash, it will be in the file as \\", but we don't want to match that)
+Regex quoteLookup = new Regex("\\\\\"(?!$)");
+
+foreach (var line in lines)
+{
+    charDiff +=
+        2 // start and end quotes
+        +
+        3 * hexCharacterLookup.Matches(line).Count // \x27 -> ' (4 characters to 1, increase diff by 3)
+        +
+        slashLookup.Matches(line).Count // \\ -> \ (2 characters to 1, increase diff by 1)
+        +
+        quoteLookup.Matches(line).Count; // \" -> " (2 characters to 1, increase diff by 1)
+
+    extraEncodedChars +=
+        2 // new start and end quotes
+        +
+        2 // new escape on old start and end quotes
+        +
+        2 * slashLookup.Matches(line).Count // \\ -> \\\\
+        +
+        2 * quoteLookup.Matches(line).Count // \" -> \\\"
+        +
+        hexCharacterLookup.Matches(line).Count; // \x26 -> \\x26
+}
+
+Console.WriteLine(charDiff);
+Console.WriteLine(extraEncodedChars);
