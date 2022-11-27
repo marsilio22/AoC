975c26e290f12195583bf2dba81530ecf9c8ed5f
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2022-11-27 22:11:21 +0000

    2015 - Day 19 PitA

--- 2015/19/Program.cs
+++ 2015/19/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.RegularExpressions;
-
-var lines = File.ReadAllLines("./input.txt");
+﻿var lines = File.ReadAllLines("./input.txt");
 
 var replacements = new Dictionary<string, ICollection<string>>();
 
@@ -9,7 +7,7 @@ foreach (var line in lines)
     if (line.Contains("=>"))
     {
         var split = line.Split(" => ", StringSplitOptions.TrimEntries);
-        
+
         if (!replacements.TryGetValue(split[0], out var repl))
         {
             repl = new List<string>();
@@ -27,7 +25,7 @@ Console.WriteLine();
 
 var newMols = new HashSet<string>();
 
-foreach(var repl in replacements)
+foreach (var repl in replacements)
 {
     for (int i = 0; i < target.Length;)
     {
@@ -38,14 +36,83 @@ foreach(var repl in replacements)
             break;
         }
 
-        foreach(var m in repl.Value)
+        foreach (var m in repl.Value)
         {
             var newmol = target.Substring(0, index) + m + target.Substring(index + repl.Key.Length);
             newMols.Add(newmol);
         }
 
-        i = index+1;
+        i = index + 1;
     }
 }
 
-Console.WriteLine(newMols.Count()); // 112 too low
+Console.WriteLine(newMols.Count());
+
+var reverseDict = replacements.SelectMany(c => c.Value.Select(d => (c.Key, d))).ToDictionary(c => c.d, c => c.Key);
+
+var groups = reverseDict.GroupBy(d => d.Key.Contains(d.Value + d.Value)).ToDictionary(d => d.Key, d => d.ToList());
+
+var multipliers = groups[true].ToDictionary(d => d.Key, d => d.Value);
+var nonMultipliers = groups[false].ToDictionary(d => d.Key, d => d.Value);
+
+groups = nonMultipliers.GroupBy(d => d.Key.Contains(d.Value)).ToDictionary(d => d.Key, d => d.ToList());
+
+var selfContainers = groups[true].ToDictionary(d => d.Key, d => d.Value);
+var nonSelfContainers = groups[false].ToDictionary(d => d.Key, d => d.Value);
+
+Console.WriteLine();
+
+var count = 0;
+
+while (target != "e")
+{
+    while (multipliers.Any(s => target.Contains(s.Key)))
+    {
+        foreach (var repl in multipliers)
+        {
+            var index = target.IndexOf(repl.Key);
+
+            if (index == -1)
+            {
+                break;
+            }
+            
+            target = target.Substring(0, index) + repl.Value + target.Substring(index + repl.Key.Length);
+            count++;
+        }
+    }
+
+    if (selfContainers.Any(s => target.Contains(s.Key)))
+    {
+        var replacer = selfContainers.First(d => target.Contains(d.Key));
+
+        var index = target.IndexOf(replacer.Key);
+
+        if (index == -1)
+        {
+            break;
+        }
+        
+        target = target.Substring(0, index) + replacer.Value + target.Substring(index + replacer.Key.Length);
+        count++;
+        continue;
+    }
+
+    if (nonSelfContainers.Any(s => target.Contains(s.Key)))
+    {
+        var replacer = nonSelfContainers.First(d => target.Contains(d.Key));
+
+        var index = target.IndexOf(replacer.Key);
+
+        if (index == -1)
+        {
+            break;
+        }
+        
+        target = target.Substring(0, index) + replacer.Value + target.Substring(index + replacer.Key.Length);
+        count++;
+        continue;
+    }
+}
+
+Console.WriteLine(count);
