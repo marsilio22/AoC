e34043c87ed3e4d0671477f4de203f6b0ab18943
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2022-12-01 05:52:16 +0000

    2022 - Day 1
    Remembered to call it 01 this time :D

---
+++ 2022/01/Program.cs
@@ -0,0 +1,22 @@
+﻿var lines = File.ReadAllLines("./input.txt");
+
+List<long> elves = new List<long>{0};
+
+foreach(var line in lines)
+{
+if (line.Equals(string.Empty))
+{
+    elves.Add(0);
+}
+else
+{
+    elves[elves.Count()-1] += int.Parse(line);
+}
+
+}
+
+
+elves = elves.OrderByDescending(c => c).ToList();
+
+Console.WriteLine(elves[0]);
+Console.WriteLine(elves[0]+ elves[1]+ elves[2]);
