4bb52d706c2b7c513b1650f7a01589b6b7fa3da9
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2022-12-02 22:28:17 +0000

    2016 - Day 12

---
+++ 2016/12/Program.cs
@@ -0,0 +1,22 @@
+﻿var input = File.ReadAllLines("./input.txt");
+
+(int a, int b, int c, int d) regs = (0, 0, 0, 0);
+
+for (int i = 0; i<input.Length; i++)
+{
+    var line = input[i].Split(" ");
+
+    switch(line[0])
+    {
+        case "cpy":
+            
+            break;
+        case "inc":
+            break;
+        case "dec":
+            break;
+        case "jnz":
+            break;
+        default: throw new Exception();
+    }
+}
