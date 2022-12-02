f6973d81a1681ac54611fb42e247d3a77f09e322
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2022-12-02 05:58:54 +0000

    2022 - Day 2

---
+++ 2022/02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO.Compression;
+
+var lines = File.ReadAllLines("./input.txt");
+
+var points = 0;
+
+foreach (var line in lines)
+{
+    var them = line[0];
+    var me = line[2];
+
+    points += me switch
+    {
+        'X' => 1 + them == 'C' ? 6 : them == 'A' ? 3 : 0,
+        'Y' => 2 + them == 'A' ? 6 : them == 'B' ? 3 : 0,
+        _ => 3 + them == 'B' ? 6 : them == 'C' ? 3 : 0
+    };
+}
+
+Console.WriteLine(points);
+
+points = 0;
+foreach (var line in lines)
+{
+    var them = line[0];
+    var me = line[2];
+
+    points += me
+    switch
+    {
+        'X' => them switch 
+        {
+            'A' => 3,
+            'B' => 1,
+            _ => 2
+        },
+        'Y' => 3 + them switch
+        {
+            'A' => 1,
+            'B' => 2,
+            _ => 3
+        },
+        _ => 6 + them switch
+        {
+            'A' => 2,
+            'B' => 3,
+            _ => 1
+        }
+    };
+}
+
+Console.WriteLine(points);
