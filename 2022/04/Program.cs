4a27e7701ba446396f24dd37c54b48896f9390f2
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2022-12-04 09:44:00 +0000

    2022 - Day 4

---
+++ 2022/04/Program.cs
@@ -0,0 +1,36 @@
+﻿var lines = File.ReadAllLines("./input.txt");
+
+var totallyContained = 0;
+var partiallyContained = 0;
+
+foreach(var line in lines)
+{
+    var split = line.Split(",");
+
+    var elf1 = split[0].Split("-").Select(e => int.Parse(e));
+    var elf2 = split[1].Split("-").Select(e => int.Parse(e));
+
+    var elf1Min = elf1.First();
+    var elf1Max = elf1.Last();
+
+    var elf2Min = elf2.First();
+    var elf2Max = elf2.Last();
+
+    if (elf1Min >= elf2Min && elf1Max <= elf2Max ||
+        elf2Min >= elf1Min && elf2Max <= elf1Max)
+    {
+        totallyContained++;
+    }
+
+    if (elf1Min >= elf2Min && elf1Min <= elf2Max ||
+        elf1Max <= elf2Max && elf1Max >= elf2Min ||
+
+        elf2Min >= elf1Min && elf2Min <= elf1Max ||
+        elf2Max <= elf1Max && elf2Max >= elf1Min)
+        {
+            partiallyContained++;
+        }
+}
+
+Console.WriteLine(totallyContained);
+Console.WriteLine(partiallyContained);
