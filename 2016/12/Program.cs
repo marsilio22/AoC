e96f592aec97e93e4e932c8ba719d84a23e4ee71
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2022-12-05 18:08:39 +0000

    2016 - Day 12

--- 2016/12/Program.cs
+++ 2016/12/Program.cs
@@ -1,22 +1,57 @@
-﻿var input = File.ReadAllLines("./input.txt");
+﻿
+using System.Security.Authentication;
 
-(int a, int b, int c, int d) regs = (0, 0, 0, 0);
+var regs = new Dictionary<string, int>{
+    ["a"] = 0,
+    ["b"] = 0,
+    ["c"] = 0,
+    ["d"] = 0
+};
+Calculate(regs);
+Console.WriteLine(regs["a"]);
 
-for (int i = 0; i<input.Length; i++)
+regs = new Dictionary<string, int>{
+    ["a"] = 0,
+    ["b"] = 0,
+    ["c"] = 1,
+    ["d"] = 0
+};
+Calculate(regs);
+Console.WriteLine(regs["a"]);
+
+void Calculate(Dictionary<string, int> regs)
 {
-    var line = input[i].Split(" ");
+    var input = File.ReadAllLines("./input.txt");
 
-    switch(line[0])
+    for (int i = 0; i < input.Length; i++)
     {
-        case "cpy":
-            
-            break;
-        case "inc":
-            break;
-        case "dec":
-            break;
-        case "jnz":
-            break;
-        default: throw new Exception();
+        var line = input[i].Split(" ");
+
+        switch(line[0])
+        {
+            case "cpy":
+                if (int.TryParse(line[1], out int blah))
+                {
+                    regs[line[2]] = blah;
+                }
+                else
+                {
+                    regs[line[2]] = regs[line[1]];
+                }
+                break;
+            case "inc":
+                regs[line[1]] = regs[line[1]] + 1;
+                break;
+            case "dec":
+                regs[line[1]] = regs[line[1]] - 1;
+                break;
+            case "jnz":
+                if (int.TryParse(line[1], out blah) && blah > 0 || regs[line[1]] > 0)
+                {
+                    i += int.Parse(line[2]) - 1;
+                }
+                break;
+            default: throw new Exception();
+        }
     }
 }
