2e45d57c7e4fb0eb914ce1916bf05379f3e68919
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2022-12-06 05:30:18 +0000

    2022 - Day 6

---
+++ 2022/06/Program.cs
@@ -0,0 +1,24 @@
+﻿var input = File.ReadAllText("./input.txt");
+
+Console.WriteLine(FindConsecutiveChars(input, 4));
+Console.WriteLine(FindConsecutiveChars(input, 14));
+
+int FindConsecutiveChars(string input, int count)
+{
+    for (int i = count; i < input.Length; i ++)
+    {
+        var h = new HashSet<char>();
+
+        for (int j = 1; j <= count; j++)
+        {
+            h.Add(input[i-j]);
+        }
+
+        if (h.Count == count)
+        {
+            return i;
+        }
+    }
+
+    throw new Exception();
+}
