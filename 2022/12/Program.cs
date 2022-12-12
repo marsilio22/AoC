90f9501b3339afc0777c388c0955b3638749d7f3
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2022-12-12 16:46:33 +0000

    2022 - Day 12 more perf

--- 2022/12/Program.cs
+++ 2022/12/Program.cs
@@ -1,10 +1,4 @@
-﻿using System.Diagnostics;
-
-var input = File.ReadAllLines("./input");
-
-var sw = new Stopwatch();
-
-sw.Start();
+﻿var input = File.ReadAllLines("./input");
 
 var map = new Dictionary<(int x, int y), (char height, int distance)>();
 
@@ -16,61 +10,17 @@ for (int j = 0; j < input.Length; j++)
     }
 }
 
-var start = map.Single(d => d.Value.height == 'S');
-var target = map.Single(d => d.Value.height == 'E');
-
-map[start.Key] = ('a', 0);
-map[target.Key] = ('z', int.MaxValue);
-
-var visited = new HashSet<(int, int)>{start.Key};
-var noMoreValidNeighbours = new HashSet<(int, int)>();
-
-while(!visited.Contains(target.Key))
-{
-    var visitedKVPs = map.Where(m => visited.Contains(m.Key) && !noMoreValidNeighbours.Contains(m.Key));
-    
-    foreach(var kvp in visitedKVPs)
-    {
-        var neighbours = Neighbours(map, kvp.Key).Where(n => n.Value.height - kvp.Value.height <= 1 && n.Value.distance > kvp.Value.distance + 1);
-
-        if (!neighbours.Any())
-        {
-            noMoreValidNeighbours.Add(kvp.Key);
-        }
-
-        foreach(var n in neighbours)
-        {
-            map[n.Key] = (n.Value.height, kvp.Value.distance + 1);
-            visited.Add(n.Key);
-        }
-    }
-}
-
-Console.WriteLine(map[target.Key].distance);
-Console.WriteLine(sw.ElapsedMilliseconds);
-
-// p2
-map = new Dictionary<(int x, int y), (char height, int distance)>();
-
-for (int j = 0; j < input.Length; j++)
-{
-    for (int i = 0; i < input[j].Length; i++)
-    {
-        map[(i, j)] = (input[j][i], int.MaxValue);
-    }
-}
-
 var S = map.Single(d => d.Value.height == 'S');
-start = map.Single(d => d.Value.height == 'E');
+var E = map.Single(d => d.Value.height == 'E');
 
 map[S.Key] = ('a', int.MaxValue);
-map[start.Key] = ('z', 0);
+map[E.Key] = ('z', 0);
 
-visited = new HashSet<(int, int)>{start.Key};
-noMoreValidNeighbours = new HashSet<(int, int)>();
+var visited = new HashSet<(int, int)>{E.Key};
+var noMoreValidNeighbours = new HashSet<(int, int)>();
 var visitedLastRound = 0;
 
-
+// take advantage of knowing what part 2 is, search backwards from the end just once, then p1 is the distance to S, and p2 is the closest 'a'
 while(visited.Count() > visitedLastRound)
 {
     var visitedKVPs = map.Where(m => visited.Contains(m.Key) && !noMoreValidNeighbours.Contains(m.Key));
@@ -81,7 +31,9 @@ while(visited.Count() > visitedLastRound)
         var neighbours = Neighbours(map, kvp.Key).Where(n => kvp.Value.height - n.Value.height <= 1 && n.Value.distance > kvp.Value.distance + 1);
 
         if (!neighbours.Any())
-        {
+        { 
+            // could probably change this so that we don't need the noMoreValidNeighbours collection, 
+            // but loop condition would need to change too and I don't want to think about it
             noMoreValidNeighbours.Add(kvp.Key);
         }
         
@@ -93,14 +45,10 @@ while(visited.Count() > visitedLastRound)
     }
 }
 
+Console.WriteLine(map[S.Key].distance);
+
 var ans = map.Where(m => m.Value.height == 'a').Min(m => m.Value.distance);
-
 Console.WriteLine(ans);
-
-Console.WriteLine(sw.ElapsedMilliseconds);
-
-
-
 
 ICollection<KeyValuePair<(int x, int y), (char height, int distance)>> Neighbours(IDictionary<(int x, int y), (char height, int distance)> map, (int x, int y) pos)
 {
