c2ee0e3e2ba79b4ccdb2e62a0e7a366993f8286d
marsilio22 <45627445+marsilio22@users.noreply.github.com>
2022-12-16 13:50:41 +0000

    2022 - Day 16 p2 nightmare is over

--- 2022/16/Program.cs
+++ 2022/16/Program.cs
@@ -1,8 +1,8 @@
-﻿var lines = File.ReadAllLines("./input2");
+﻿var lines = File.ReadAllLines("./input");
 
 IDictionary<string, Valve> valves = new Dictionary<string, Valve>();
 
-foreach(var line in lines)
+foreach (var line in lines)
 {
     var split = line.Split("; ");
 
@@ -19,22 +19,22 @@ foreach(var line in lines)
     valves.Add(val.Name, val);
 }
 
-foreach(var thing in valves)
+foreach (var thing in valves)
 {
     var algLocation = thing.Key;
 
-    Dictionary<string, (int distance, List<string> path)> worths = 
+    Dictionary<string, (int distance, List<string> path)> worths =
         valves.ToDictionary(v => v.Key, v => (int.MaxValue, new List<string>()));
     var unvisited = valves.Keys.ToList();
 
     worths[algLocation] = (0, new List<string>());
 
-    while(unvisited.Any())
+    while (unvisited.Any())
     {
         algLocation = worths.Where(w => unvisited.Contains(w.Key)).MinBy(w => w.Value.distance).Key;
         var currentWorth = worths[algLocation];
 
-        foreach(var neighbour in valves[algLocation].Neighbours)
+        foreach (var neighbour in valves[algLocation].Neighbours)
         {
             var val = worths[neighbour];
 
@@ -47,14 +47,14 @@ foreach(var thing in valves)
 
         unvisited.Remove(algLocation);
     }
-    
+
     thing.Value.Distances = worths;
 }
 
-var nonzerovalves = valves.Where(v => v.Value.Flow != 0).ToDictionary(k => k.Key, k => k.Value);
+int _best = 0;
 
 Console.WriteLine(Recurse(valves, "AA", 0, 30));
-Console.WriteLine(Recurse2(valves, "AA", "AA", 0, 26));
+Console.WriteLine(Recurse2(valves, "AA", "AA", "", "", 0, 26));
 
 int Recurse(IDictionary<string, Valve> valveMap, string current, int score, int remainingTime)
 {
@@ -68,13 +68,13 @@ int Recurse(IDictionary<string, Valve> valveMap, string current, int score, int 
 
     var best = 0;
 
-    foreach(var valve in unopened)
+    foreach (var valve in unopened)
     {
         var d = valve.Value.Distances[current];
         var s = score + (d.distance + 1) * scorePerTime;
         var remaining = remainingTime - (d.distance + 1);
 
-        var newMap = valveMap.ToDictionary(v => v.Key, v => v.Value with {});
+        var newMap = valveMap.ToDictionary(v => v.Key, v => v.Value with { });
         newMap[valve.Key].Open = true;
 
         best = Math.Max(Recurse(newMap, valve.Key, s, remaining), best);
@@ -83,47 +83,48 @@ int Recurse(IDictionary<string, Valve> valveMap, string current, int score, int 
     return best;
 }
 
-int Recurse2(IDictionary<string, Valve> valveMap, string currentMe, string currentEle, int score, int remainingTime)
+int Recurse2(IDictionary<string, Valve> valveMap, string currentMe, string currentEle, string currentMeTarget, string currentEleTarget, int score, int remainingTime)
 {
-    var unopened = valveMap.Where(v => v.Value.Flow > 0 && !v.Value.Open && (v.Value.Distances[currentMe].distance + 1 < remainingTime || v.Value.Distances[currentEle].distance + 1 < remainingTime)).ToList();
+    var unopened = valveMap.Where(v => v.Value.Flow > 0 && !v.Value.Open && (v.Value.Distances[currentMe].distance + 1 < remainingTime || v.Value.Distances[currentEle].distance + 1 < remainingTime)).OrderByDescending(d => d.Value.Flow).ToList();
     var scorePerTime = valveMap.Values.Where(v => v.Open).Sum(v => v.Flow);
 
     if (!unopened.Any())
     {
-        return score + remainingTime * scorePerTime;
+        var b = score + remainingTime * scorePerTime;
+        return b;
     }
 
     var best = 0;
 
-    foreach(var meValve in unopened)
+    var meMax = string.IsNullOrEmpty(currentMeTarget) ? unopened.Count : 1;
+    var eleMax = string.IsNullOrEmpty(currentEleTarget) ? unopened.Count : 1;
+
+    for (int me = 0; me < meMax; me++)
     {
-        foreach(var eleValve in unopened)
+        var meValve = string.IsNullOrEmpty(currentMeTarget) ? unopened[me] : valveMap.Single(v => v.Key.Equals(currentMeTarget));
+        for (int ele = 0; ele < eleMax; ele++)
         {
-            if (meValve.Key == eleValve.Key)
+            var eleValve = string.IsNullOrEmpty(currentEleTarget) ? unopened[ele] : valveMap.Single(v => v.Key.Equals(currentEleTarget));
+
+            if (meValve.Key.Equals(eleValve.Key) && meMax != 1)
             {
                 continue;
             }
-
-            // todo
-            // figure out whether me or the elephant is closest to their valve
-            // move to that valve, and the path location which is that far along for the other person
-            // recurse with those new locations/score
-            // hope for the best
 
             var dMe = meValve.Value.Distances[currentMe].distance;
             var dEle = eleValve.Value.Distances[currentEle].distance;
 
             var winner = Math.Min(dMe, dEle);
 
-            var mePath = meValve.Value.Distances[currentMe].path;
-            var elePath = eleValve.Value.Distances[currentEle].path;
+            var mePath = valveMap[currentMe].Distances[meValve.Key].path;
+            var elePath = valveMap[currentEle].Distances[eleValve.Key].path;
 
             string mePos, elePos;
 
             if (winner != 0)
             {
-                mePos = dMe == winner ? meValve.Key : mePath[mePath.Count - (winner)];
-                elePos = dEle == winner ? eleValve.Key : elePath[elePath.Count - (winner)];
+                mePos = dMe == winner ? meValve.Key : mePath[winner - 1];
+                elePos = dEle == winner ? eleValve.Key : elePath[winner - 1];
             }
             else
             {
@@ -131,36 +132,89 @@ int Recurse2(IDictionary<string, Valve> valveMap, string currentMe, string curre
                 elePos = currentEle;
             }
 
-            // var mePos = meValve.Value.Distances[currentMe].path[Math.Min(dMe-1, Math.Max(winner-1, 0))]; // + 1???
-            // var elePos = eleValve.Value.Distances[currentEle].path[Math.Min(dEle-1, Math.Max(winner-1, 0))]; // might end up out of bounds or something
-
             var s = score + (winner + 1) * scorePerTime;
             var remaining = remainingTime - (winner + 1);
 
-            var newMap = valveMap.ToDictionary(v => v.Key, v => v.Value with {});
+            var newMap = valveMap.ToDictionary(v => v.Key, v => v.Value with { });
+
+            string meTarget = string.Empty;
+            string eleTarget = string.Empty;
 
             if (mePos.Equals(meValve.Key))
             {
                 newMap[meValve.Key].Open = true;
             }
-            else 
+            else
             {
                 // move one more on, because the elephant will open it's valve
-                mePos = mePath[mePath.Count - winner - 1];
+                mePos = mePath[(winner)];
+                meTarget = meValve.Key;
             }
 
             if (elePos.Equals(eleValve.Key))
             {
                 newMap[eleValve.Key].Open = true;
             }
-            else 
+            else
             {
                 // move one more on, because me will open me's valve
-                elePos = elePath[elePath.Count - winner - 1];
+                elePos = elePath[(winner)];
+                eleTarget = eleValve.Key;
             }
 
-            best = Math.Max(Recurse2(newMap, mePos, elePos, s, remaining), best);
+            //if (new (int, int)[]
+            //        { (20, 23), (61, 22), (184, 19), (260, 18), (336, 17), (414, 16), (492, 15), (573, 14) }
+            //    .Contains((s, remaining)))
+            //{
+            //    Console.WriteLine();
+            //}
+
+            //if (new (int, int)[]
+            //        { (20, 22), (61, 21), (184, 18), (260, 17), (336, 16), (414, 15), (492, 14), (573, 13) }
+            //    .Contains((s, remaining)))
+            //{
+            //    Console.WriteLine();
+            //}
+
+            //if (new[]
+            //    {
+            //        //(23, 20),
+            //        //(22, 61),
+            //        //(21, 102),
+            //        //(20, 143),
+            //        //(19, 184),
+            //        //(18, 260),
+            //        (17, 336),
+            //        (16, 414),
+            //        (15, 492),
+            //        (14, 573),
+            //        (13, 654),
+            //        (12, 735),
+            //        (11, 816),
+            //        (10, 897),
+            //        (9, 978),
+            //        (8, 1059),
+            //        (7, 1140),
+            //        (6, 1221),
+            //        (5, 1302),
+            //        (4, 1383),
+            //        (3, 1464),
+            //        (2, 1545),
+            //        (1, 1626),
+            //        (0, 1707)
+            //    }.Contains((remaining, s)))
+            //{
+            //    Console.WriteLine();
+            //}
+
+            best = Math.Max(Recurse2(newMap, mePos, elePos, meTarget, eleTarget, s, remaining), best);
         }
+    }
+
+    if (best > _best)
+    {
+        _best = best;
+        Console.WriteLine(best); // 2085 too low // 2114 too low // 2351 correct
     }
 
     return best;
@@ -211,7 +265,7 @@ int Recurse2(IDictionary<string, Valve> valveMap, string currentMe, string curre
 
 //         unvisited.Remove(algLocation);
 //     }
-    
+
 //     var best = worths.MaxBy(w => w.Value.worth);
 //     var best2 = worths.Where(w => w.Value.worth > 0).OrderBy(w => w.Value.distance).ThenByDescending(w => w.Value.worth).FirstOrDefault();
 //     var best3 = worths.OrderByDescending(w => w.Value.worth).Take(3).MaxBy(w => w.Value.distance);
@@ -233,14 +287,10 @@ int Recurse2(IDictionary<string, Valve> valveMap, string currentMe, string curre
 //     }
 // }
 
-
-
 // Console.WriteLine(ans); // 1870 too high // 1768 too low // 1769 too low
 
-
-
-
-record Valve {
+record Valve
+{
     public string Name { get; set; }
 
     public int Flow { get; set; }
